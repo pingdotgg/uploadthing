42cc72d16991e79e1c6b66ad82008f9b3968e89b
Abhijit Roy <roya51788@gmail.com>
2023-05-11 18:30:25 +0530

    enable plugin in your ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,10 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `@uploadthing/eslint-config`
-  extends: ["@uploadthing"],
+  extends: [
+    "@uploadthing",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
   settings: {
     next: {
       rootDir: ["examples/*/"],
