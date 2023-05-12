6c094889055df8048af064bbac86279effe514fc
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-05-11 23:17:24 -0700

    add dark mode to docs

--- docs/src/theme.config.js
+++ docs/src/theme.config.js
@@ -54,44 +54,45 @@ body {
 `;
 
 const Head = () => {
-  const { asPath, defaultLocale, locale } = useRouter();
-  const { frontMatter } = useConfig();
-  const url =
-    "https://my-app.com" +
-    (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+        "https://my-app.com" +
+        (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
 
-  return (
-    <>
-      <meta property="og:url" content={url} />
-      <meta
-        property="og:title"
-        content={frontMatter.title || "uploadthing docs"}
-      />
-      <style>{hackedCss}</style>
-      <meta
-        property="og:description"
-        content={
-          frontMatter.description || "Docs for the best file uploader to date"
-        }
-      />
-      <meta
-        name="twitter:image"
-        content="https://docs.uploadthing.com/og.jpg"
-      />
-    </>
-  );
+    return (
+        <>
+            <meta property="og:url" content={url} />
+            <meta
+                property="og:title"
+                content={frontMatter.title || "uploadthing docs"}
+            />
+            <style>{hackedCss}</style>
+            <meta
+                property="og:description"
+                content={
+                    frontMatter.description ||
+                    "Docs for the best file uploader to date"
+                }
+            />
+            <meta
+                name="twitter:image"
+                content="https://docs.uploadthing.com/og.jpg"
+            />
+        </>
+    );
 };
 
 export const Logo = () => {
-  return (
-    <h1 className="flex flex-row items-baseline text-2xl font-bold">
-      <span className="tracking-tight hover:cursor-pointer">
-        {`upload`}
-        <span className="text-red-600">{`thing`}</span>
-        <span className="ml-1 font-semibold">docs</span>
-      </span>
-    </h1>
-  );
+    return (
+        <h1 className="flex flex-row items-baseline text-2xl font-bold">
+            <span className="dark:text-white tracking-tight hover:cursor-pointer">
+                {`upload`}
+                <span className="text-red-600">{`thing`}</span>
+                <span className="ml-1 font-semibold">docs</span>
+            </span>
+        </h1>
+    );
 };
 
 /* eslint sort-keys: error */
@@ -99,67 +100,69 @@ export const Logo = () => {
  * @type {import('nextra-theme-docs').DocsThemeConfig}
  */
 const config = {
-  chat: {
-    link: "https://discord.gg/UCXkw6xj2K",
-  },
-  darkMode: false,
-  editLink: {
-    component: () => null,
-  },
-  feedback: {
-    content: () => null,
-  },
-  footer: { component: () => null },
-  head: Head,
-  logo: Logo,
-  nextThemes: {
-    forcedTheme: "light",
-  },
-  primaryHue: 348,
-  useNextSeoProps() {
-    return {
-      additionalLinkTags: [
-        {
-          href: "/apple-icon-180x180.png",
-          rel: "apple-touch-icon",
-          sizes: "180x180",
-        },
-        {
-          href: "/android-icon-192x192.png",
-          rel: "icon",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          href: "/favicon-32x32.png",
-          rel: "icon",
-          sizes: "32x32",
-          type: "image/png",
-        },
-        {
-          href: "/favicon-16x16.png",
-          rel: "icon",
-          sizes: "16x16",
-          type: "image/png",
-        },
-      ],
-      additionalMetaTags: [
-        { content: "en", httpEquiv: "Content-Language" },
-        { content: "Nextra", name: "apple-mobile-web-app-title" },
-      ],
-      description: "Docs for the best file uploader to date",
-      openGraph: {
-        images: [
-          { url: "https://docs.uploadthing.com/og.jpg?random=aaaaaaaaaaaaa" },
-        ],
-      },
-      titleTemplate: "%s – uploadthing",
-      twitter: {
-        cardType: "summary_large_image",
-        site: "https://docs.uploadthing.com",
-      },
-    };
-  },
+    chat: {
+        link: "https://discord.gg/UCXkw6xj2K",
+    },
+    darkMode: true,
+    editLink: {
+        component: () => null,
+    },
+    feedback: {
+        content: () => null,
+    },
+    footer: { component: () => null },
+    head: Head,
+    logo: Logo,
+    nextThemes: {
+        defaultTheme: "system",
+    },
+    primaryHue: 348,
+    useNextSeoProps() {
+        return {
+            additionalLinkTags: [
+                {
+                    href: "/apple-icon-180x180.png",
+                    rel: "apple-touch-icon",
+                    sizes: "180x180",
+                },
+                {
+                    href: "/android-icon-192x192.png",
+                    rel: "icon",
+                    sizes: "192x192",
+                    type: "image/png",
+                },
+                {
+                    href: "/favicon-32x32.png",
+                    rel: "icon",
+                    sizes: "32x32",
+                    type: "image/png",
+                },
+                {
+                    href: "/favicon-16x16.png",
+                    rel: "icon",
+                    sizes: "16x16",
+                    type: "image/png",
+                },
+            ],
+            additionalMetaTags: [
+                { content: "en", httpEquiv: "Content-Language" },
+                { content: "Nextra", name: "apple-mobile-web-app-title" },
+            ],
+            description: "Docs for the best file uploader to date",
+            openGraph: {
+                images: [
+                    {
+                        url: "https://docs.uploadthing.com/og.jpg?random=aaaaaaaaaaaaa",
+                    },
+                ],
+            },
+            titleTemplate: "%s – uploadthing",
+            twitter: {
+                cardType: "summary_large_image",
+                site: "https://docs.uploadthing.com",
+            },
+        };
+    },
 };
 
 export default config;
