fbc5d63abb0f1324801bef92abc31efc6739d5cb
Yash Singh <saiansh2525@gmail.com>
2023-05-21 20:05:33 -0700

    chore: rm turborepo example

--- examples/create-t3-turbo/apps/nextjs/src/app/api/uploadthing/route.ts
+++
@@ -1,7 +0,0 @@
-import { ourFileRouter } from "./core";
-import { createNextRouteHandler } from "uploadthing/next";
-
-// Export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
-  router: ourFileRouter,
-});
--- examples/create-t3-turbo/apps/nextjs/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import baseConfig from "@acme/tailwind-config";
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./src/**/*.tsx"],
-  presets: [baseConfig],
-} satisfies Config;
