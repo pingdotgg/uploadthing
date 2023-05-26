a210c955ac6da808c89f3e913283a432c6165801
juliusmarminge <julius0216@outlook.com>
2023-05-26 21:00:27 +0200

    fix url

--- packages/uploadthing/src/internal/handler.ts
+++ packages/uploadthing/src/internal/handler.ts
@@ -127,8 +127,20 @@ const conditionalDevServer = async (fileKey: string) => {
 };
 
 const GET_DEFAULT_URL = () => {
+  /**
+   * Use VERCEL_URL as the default callbackUrl if it's set
+   * they don't set the protocol, so we need to add it
+   * User can override this with the UPLOADTHING_URL env var,
+   * if they do, they should include the protocol
+   *
+   * The pathname must be /api/uploadthing
+   * since we call that via webhook, so the user
+   * should not override that. Just the protocol and host
+   */
   const vcurl = process.env.VERCEL_URL;
   if (vcurl) return `https://${vcurl}/api/uploadthing`; // SSR should use vercel url
+  const uturl = process.env.UPLOADTHING_URL;
+  if (uturl) return `${uturl}/api/uploadthing`;
 
   return `http://localhost:${process.env.PORT ?? 3000}/api/uploadthing`; // dev SSR should use localhost
 };
@@ -199,7 +211,7 @@ export const buildRequestHandler = <
     try {
       const { files } = reqBody;
       // @ts-expect-error TODO: Fix this
-      const metadata = await uploadable._def.middleware(req as Request, res);
+      const metadata = await uploadable._def.middleware(req, res);
 
       // Once that passes, persist in DB
 
