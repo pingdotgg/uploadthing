bb9d9b7ef1fee5dbce4688b360ce410818ac372b
juliusmarminge <julius0216@outlook.com>
2023-05-26 20:47:56 +0200

    fixy

--- packages/uploadthing/src/constants.ts
+++ packages/uploadthing/src/constants.ts
@@ -1,6 +1,6 @@
 import type { PackageJson } from "type-fest";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require("../../package.json") as PackageJson;
+const packageJson = require("../package.json") as PackageJson;
 if (!packageJson.version) throw new Error("no version found in package.json");
 export const UPLOADTHING_VERSION = packageJson.version;
