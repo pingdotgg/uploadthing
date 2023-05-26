65f5408a68134fa4bff93800e916b4d4a89ff030
Marko <markoni521@gmail.com>
2023-05-26 20:06:05 +0200

    Removed @ts-expect-error for middleware in upload-builder.ts (#99)
    * Removed @ts-expect-error for middleware in upload-builder.ts
    
    * Changed to the other 'MiddlewareFn' type. Rearranged type locations in code for easier understanding. Renamed 'Args' type to 'MiddlewareFnArgs'.
    
    * cleanup in tests
    
    ---------
    
    Co-authored-by: juliusmarminge <julius0216@outlook.com>

--- packages/uploadthing/src/types.ts
+++ packages/uploadthing/src/types.ts
@@ -39,25 +39,28 @@ type ResolverOptions<TParams extends AnyParams> = {
   file: UploadedFile;
 };
 
-export type ReqMiddlewareFn<TOutput extends Record<string, unknown>> = (
-  req: Request
-) => MaybePromise<TOutput>;
-export type NextReqMiddlewareFn<TOutput extends Record<string, unknown>> = (
-  req: NextRequest
-) => MaybePromise<TOutput>;
-export type NextApiMiddlewareFn<TOutput extends Record<string, unknown>> = (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => MaybePromise<TOutput>;
+type MiddlewareFnArgs<TRuntime> = TRuntime extends "web"
+  ? { req: Request; res: never }
+  : TRuntime extends "app"
+  ? { req: NextRequest; res: never }
+  : { req: NextApiRequest; res: NextApiResponse };
 
 type MiddlewareFn<
   TOutput extends Record<string, unknown>,
   TRuntime extends string
-> = TRuntime extends "web"
-  ? ReqMiddlewareFn<TOutput>
-  : TRuntime extends "app"
-  ? NextReqMiddlewareFn<TOutput>
-  : NextApiMiddlewareFn<TOutput>;
+> = MiddlewareFnArgs<TRuntime>["res"] extends never
+  ? (req: MiddlewareFnArgs<TRuntime>["req"]) => MaybePromise<TOutput>
+  : (
+      req: MiddlewareFnArgs<TRuntime>["req"],
+      res: MiddlewareFnArgs<TRuntime>["res"]
+    ) => MaybePromise<TOutput>;
+
+export type ReqMiddlewareFn<TOutput extends Record<string, unknown>> =
+  MiddlewareFn<TOutput, "web">;
+export type NextReqMiddlewareFn<TOutput extends Record<string, unknown>> =
+  MiddlewareFn<TOutput, "app">;
+export type NextApiMiddlewareFn<TOutput extends Record<string, unknown>> =
+  MiddlewareFn<TOutput, "pages">;
 
 type ResolverFn<TParams extends AnyParams> = (
   opts: ResolverOptions<TParams>
