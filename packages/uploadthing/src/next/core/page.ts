bb8b244c578a69984cb651dfb1a59fb0c84e227b
juliusmarminge <julius0216@outlook.com>
2023-05-28 10:44:03 +0200

    json parse

--- packages/uploadthing/src/next/core/page.ts
+++ packages/uploadthing/src/next/core/page.ts
@@ -36,10 +36,12 @@ export const createNextPageApiHandler = <TRouter extends FileRouter>(
       return res.status(400).send("`actionType` must not be an array");
     if (uploadthingHook && typeof uploadthingHook !== "string")
       return res.status(400).send("`uploadthingHook` must not be an array");
+    if (typeof req.body !== "string")
+      return res.status(400).send("Request body must be a JSON string");
 
     const standardRequest = {
       ...req,
-      json: () => Promise.resolve(req.body),
+      json: () => Promise.resolve(JSON.parse(req.body)),
       headers: {
         get: (key: string) => req.headers[key],
       } as Headers,
