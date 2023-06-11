ee57bfc099db07f52fa24a00767c2c5c075d2d33
juliusmarminge <julius0216@outlook.com>
2023-06-11 20:38:30 +0200

    dont typecheck twice

--- examples/appdir/next.config.js
+++ examples/appdir/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["uploadthing", "@uploadthing/react"],
   eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 module.exports = nextConfig;
