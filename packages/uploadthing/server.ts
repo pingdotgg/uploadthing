9797f5182351caaaacd88e12f187547937667b44
OrJDev <91349014+OrJDev@users.noreply.github.com>
2023-06-21 00:32:55 +0300

    fix: make sure running info only prints on server & solid server code doesn't leak (#155)

--- packages/uploadthing/server.ts
+++ packages/uploadthing/server.ts
@@ -2,5 +2,6 @@ import { createBuilder } from "./src/upload-builder";
 
 export * from "./src/internal/types";
 export * as utapi from "./src/helper-functions";
+export { createServerHandler } from "./src/internal/edge";
 
 export const createUploadthing = () => createBuilder<"web">();
