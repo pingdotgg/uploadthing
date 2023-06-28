bd32e2c41c06b9d462899289b0cb786629dc32e9
juliusmarminge <julius0216@outlook.com>
2023-06-28 08:44:33 +0200

    that didn't work :/

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -34,12 +34,12 @@ type RouteConfig = {
 
 export type FileRouterInputKey = AllowedFileType | MimeType;
 
-export type ExpandedRouteConfig = {
-  [key in FileRouterInputKey]+?: RouteConfig;
-};
+export type ExpandedRouteConfig = Partial<{
+  [key in FileRouterInputKey]: RouteConfig;
+}>;
 
-type PartialRouteConfig = {
-  [key in FileRouterInputKey]+?: Partial<RouteConfig>;
-};
+type PartialRouteConfig = Partial<
+  Record<FileRouterInputKey, Partial<RouteConfig>>
+>;
 
 export type FileRouterInputConfig = FileRouterInputKey[] | PartialRouteConfig;
