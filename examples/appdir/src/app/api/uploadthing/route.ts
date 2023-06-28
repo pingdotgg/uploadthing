e7e6b3595825302450e39310aa49c7eb84746a2e
juliusmarminge <julius0216@outlook.com>
2023-06-28 10:47:14 +0200

    test it out a bit

--- examples/appdir/src/app/api/uploadthing/route.ts
+++ examples/appdir/src/app/api/uploadthing/route.ts
@@ -6,4 +6,11 @@ export const runtime = "edge";
 
 export const { GET, POST } = createNextRouteHandler({
   router: uploadRouter,
+  errorFormatter: (error) => {
+    return {
+      message: error.message,
+      foo: "my custom field",
+      // date: new Date(), // invalid json - should type-error
+    };
+  },
 });
