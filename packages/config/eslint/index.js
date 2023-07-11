95594684b79b406d63c98b458f0db79d9ef10f1a
juliusmarminge <julius0216@outlook.com>
2023-07-12 00:44:46 +0200

    allow async in jsx functions

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -29,6 +29,10 @@ const config = {
         disallowTypeAnnotations: false,
       },
     ],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      { checksVoidReturn: { attributes: false } },
+    ],
     "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
   },
   overrides: [
