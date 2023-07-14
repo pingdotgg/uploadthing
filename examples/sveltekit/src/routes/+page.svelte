e2427a2b183f8ec6244424d37f1322a5f904472b
Alan Acuña <alantheace@outlook.com>
2023-07-14 15:11:02 -0600

    Sveltekit support: example ready

--- examples/sveltekit/src/routes/+page.svelte
+++ examples/sveltekit/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type { OurFileRouter } from "$lib/server/uploadthing";
+
+  import { createUploader, Uploader } from "@uploadthing/svelte";
+
+  const uploader = createUploader<OurFileRouter>({
+    endpoint: "withoutMdwr",
+    onClientUploadComplete: (res) => {
+      console.log(`onClientUploadComplete`, res);
+      alert("Upload Completed");
+    },
+    onUploadError: (error: Error) => {
+      alert(`ERROR! ${error.message}`);
+    },
+  });
+</script>
+
+<main
+  class="flex min-h-screen flex-col items-center justify-center gap-16 p-24"
+>
+  <Uploader {uploader} />
+</main>
