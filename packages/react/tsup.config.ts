2e808826693336651cbae4b3009d65df682d4da5
juliusmarminge <julius0216@outlook.com>
2023-08-12 21:54:39 +0200

    nit

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from "tsup";
 
 import { config } from "@uploadthing/tsup-config";
 
-export default defineConfig((opts) => [
-  {
-    ...config,
-    entry: ["./src/index.ts", "./src/hooks.ts"],
-    clean: !opts.watch,
-    esbuildOptions: (option) => {
-      option.banner = {
-        js: `"use client";`,
-      };
-    },
-  }
-]);
+export default defineConfig((opts) => ({
+  ...config,
+  entry: ["./src/index.ts", "./src/hooks.ts"],
+  clean: !opts.watch,
+  esbuildOptions: (option) => {
+    option.banner = {
+      js: `"use client";`,
+    };
+  },
+}));
