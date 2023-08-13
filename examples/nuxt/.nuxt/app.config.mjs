638985d5f744b10712943fb4becdd819dc4db99e
Vladislav Zimnikov <vladislavzimnikov@Vladislavs-MacBook-Pro.local>
2023-08-13 20:02:32 +0300

    VueJS + NuxtJS support
    - Removed generated files from git

--- examples/nuxt/.nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/vladislavzimnikov/Projects/uploadthing/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
--- examples/nuxt/.output/server/chunks/rollup/_virtual_head-static.mjs
+++
@@ -1,4 +0,0 @@
-const _virtual__headStatic = {"headTags":"<meta charset=\"utf-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">","bodyTags":"","bodyTagsOpen":"","htmlAttrs":"","bodyAttrs":""};
-
-export { _virtual__headStatic as default };
-//# sourceMappingURL=_virtual_head-static.mjs.map
