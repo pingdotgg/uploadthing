6812f4b2b7321850c946695a2e341a12a54c4875
juliusmarminge <julius0216@outlook.com>
2023-08-14 21:48:33 +0200

    fix darkmode

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -1,6 +1,7 @@
 import { withUt } from "uploadthing/tw";
 
 export default withUt({
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,md,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
