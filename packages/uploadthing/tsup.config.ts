a596a0802838a499e7ada6c67e62e5b84eb10058
juliusmarminge <julius0216@outlook.com>
2023-08-27 22:52:53 +0200

    simplify

--- packages/uploadthing/tsup.config.ts
+++ packages/uploadthing/tsup.config.ts
@@ -2,16 +2,14 @@ import { defineConfig } from "tsup";
 
 import { config } from "@uploadthing/tsup-config";
 
-export default defineConfig((opts) => [
-  {
-    ...config,
-    entry: [
-      "./src/client.ts",
-      "./src/server.ts",
-      "./src/next.ts",
-      "./src/next-legacy.ts",
-      "./src/tw.ts",
-    ],
-    clean: !opts.watch,
-  },
-]);
+export default defineConfig((opts) => ({
+  ...config,
+  entry: [
+    "./src/client.ts",
+    "./src/server.ts",
+    "./src/next.ts",
+    "./src/next-legacy.ts",
+    "./src/tw.ts",
+  ],
+  clean: !opts.watch,
+}));
