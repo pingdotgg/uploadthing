bbf3b1e6853acd006c2d73da5bb9f01162725c12
Julius Marminge <julius0216@outlook.com>
2023-08-31 22:27:23 +0200

    Update uploadthing.ts

--- examples/minimal-pagedir/src/server/uploadthing.ts
+++ examples/minimal-pagedir/src/server/uploadthing.ts
@@ -19,9 +19,11 @@ export const uploadRouter = {
   })
     .middleware(({ req }) => {
       // Check some condition based on the incoming requrest
-      if (!req.headers["x-some-header"]) {
-        throw new Error("x-some-header is required");
-      }
+      req;
+      //^?
+      // if (!req.headers["x-some-header"]) {
+      //   throw new Error("x-some-header is required");
+      // }
 
       // Return some metadata to be stored with the file
       return { foo: "bar" as const };
