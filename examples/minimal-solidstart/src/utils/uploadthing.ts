57c8ad125288fc09bd5ee058b782c67168f07f73
juliusmarminge <julius0216@outlook.com>
2023-09-06 16:09:19 +0200

    proper example in solid example

--- examples/minimal-solidstart/src/utils/uploadthing.ts
+++ examples/minimal-solidstart/src/utils/uploadthing.ts
@@ -2,7 +2,9 @@ import { generateComponents, generateSolidHelpers } from "@uploadthing/solid";
 
 import type { OurFileRouter } from "~/server/uploadthing";
 
-const url = "http://localhost:9898";
+const url = import.meta.env.VITE_VERCEL_URL
+  ? `https://${import.meta.env.VITE_VERCEL_URL}/api/uploadthing`
+  : `http://localhost:${import.meta.env.VITE_PORT ?? 9898}/api/uploadthing`;
 
 export const { UploadButton, UploadDropzone } =
   generateComponents<OurFileRouter>({ url });
