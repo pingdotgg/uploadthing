074cedcf331b7d511f38e95e49374c975bd62a17
Theo Browne <me@t3.gg>
2023-09-07 17:32:19 -0700

    some documentation

--- examples/minimal-appdir/src/app/utHead.tsx
+++ examples/minimal-appdir/src/app/utHead.tsx
@@ -3,7 +3,11 @@ import Script from "next/script";
 import { eagerSetupUploadThingSSR } from "@uploadthing/shared";
 import { FileRouter } from "uploadthing/next";
 
+/**
+ * Pass this your router IN A SERVER COMPONENT and it will prefill all the data the UploadThing components need to render
+ */
 export const UTHead = (props: { router: FileRouter }) => {
+  if (typeof window !== "undefined") throw new Error("UTHead is SSR only");
   const utConfig = eagerSetupUploadThingSSR(props.router);
 
   return (
