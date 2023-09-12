a5959392ea935528a99f5af71d8da21c6a5977a7
juliusmarminge <julius0216@outlook.com>
2023-09-12 23:35:48 +0200

    rename

--- examples/with-drizzle-appdir/next.config.mjs
+++ examples/with-drizzle-appdir/next.config.mjs
@@ -7,6 +7,9 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default config;
