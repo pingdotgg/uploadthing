3bec7138dc8457876219ae2d5292258092c8b7d3
juliusmarminge <julius0216@outlook.com>
2023-09-13 00:21:41 +0200

    bump to astro 3

--- examples/minimal-astro-react/src/pages/api/uploadthing.ts
+++ examples/minimal-astro-react/src/pages/api/uploadthing.ts
@@ -2,11 +2,11 @@ import { createServerHandler } from "uploadthing/server";
 
 import { uploadRouter } from "../../server/uploadthing";
 
-export const { GET: get, POST: post } = createServerHandler({
+export const { GET, POST } = createServerHandler({
   router: uploadRouter,
   config: {
     uploadthingId: import.meta.env.UPLOADTHING_APPID,
     uploadthingSecret: import.meta.env.UPLOADTHING_SECRET,
-    callbackUrl: "http://localhost:3000/api/uploadthing",
+    callbackUrl: "http://localhost:4321/api/uploadthing",
   },
 });
