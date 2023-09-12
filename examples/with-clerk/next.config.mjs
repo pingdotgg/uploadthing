f8f02e8a72614c33b5fe222641b5250601877b11
Vladislav Zimnikov <vladislavzimnikov@Vladislavs-MacBook-Pro.local>
2023-09-12 16:06:34 +0300

    remove redundant config

--- examples/with-clerk/next.config.mjs
+++ examples/with-clerk/next.config.mjs
@@ -7,16 +7,6 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 };
 
 export default config;
