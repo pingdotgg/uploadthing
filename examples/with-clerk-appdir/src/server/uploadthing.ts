dd3e07a1f507ee2a82d994f37541b7722a166734
Mr0Bread <55754540+Mr0Bread@users.noreply.github.com>
2023-09-13 00:33:56 +0300

    chore: example with clerk (#342)
    Co-authored-by: Vladislav Zimnikov <vladislavzimnikov@Vladislavs-MacBook-Pro.local>
    Co-authored-by: Julius Marminge <julius0216@outlook.com>

---
+++ examples/with-clerk-appdir/src/server/uploadthing.ts
@@ -0,0 +1,38 @@
+import { auth } from "@clerk/nextjs";
+
+import { createUploadthing } from "uploadthing/next";
+import type { FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+/**
+ * This is your Uploadthing file router. For more information:
+ * @see https://docs.uploadthing.com/api-reference/server#file-routes
+ */
+
+export const uploadRouter = {
+  videoAndImage: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 4,
+    },
+    video: {
+      maxFileSize: "16MB",
+    },
+  })
+    .middleware(({ req }) => {
+      const { userId } = auth();
+
+      if (!userId) {
+        throw new Error("Please sign in");
+      }
+
+      return { userId };
+    })
+    .onUploadComplete(({ file, metadata }) => {
+      metadata;
+      // ^?
+      console.log("upload completed", file);
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof uploadRouter;
