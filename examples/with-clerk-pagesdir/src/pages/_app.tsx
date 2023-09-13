85b49501c2309f99f16686f5fa7f6675e9c8894d
Vladislav Zimnikov <vladislavzimnikov@Vladislavs-MacBook-Pro.local>
2023-09-13 12:26:38 +0300

    disable prerender

--- examples/with-clerk-pagesdir/src/pages/_app.tsx
+++ examples/with-clerk-pagesdir/src/pages/_app.tsx
@@ -4,11 +4,16 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "@uploadthing/react/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
+    return (
+        <ClerkProvider {...pageProps}>
+            <Component {...pageProps} />
+        </ClerkProvider>
+    );
+}
+
+// Opt out of prerendering
+MyApp.getInitialProps = async () => {
+    return {};
 }
 
 export default MyApp;
