e6444e7e502cc34f41b7a695f72783c982d584df
Mr0Bread <zimnikovvladislav@gmail.com>
2023-09-15 00:12:37 +0300

    prettier

--- examples/with-drizzle-pagesdir/src/pages/_app.tsx
+++ examples/with-drizzle-pagesdir/src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import "@uploadthing/react/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- examples/with-drizzle-pagesdir/src/pages/index.tsx
+++ examples/with-drizzle-pagesdir/src/pages/index.tsx
@@ -1,71 +1,73 @@
 import { Inter } from "next/font/google";
 import useSWR from "swr";
-import type { File } from '~/server/db/schema'
 
+import type { File } from "~/server/db/schema";
 import { UploadButton, UploadDropzone } from "~/utils/uploadthing";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Page() {
-    const { isLoading, data } = useSWR(
-        "files",
-        () => {
-            return fetch("/api/files").then((res) => res.json()) as Promise<{ files: File[] }>
-        },
-        { refreshInterval: 300 }
-    );
+  const { isLoading, data } = useSWR(
+    "files",
+    () => {
+      return fetch("/api/files").then((res) => res.json()) as Promise<{
+        files: File[];
+      }>;
+    },
+    { refreshInterval: 300 },
+  );
 
-    return (
-        <main
-            className={inter.className}
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <UploadButton
-                /**
-                 * @see https://docs.uploadthing.com/api-reference/react#uploadbutton
-                 */
-                endpoint="videoAndImage"
-                onClientUploadComplete={async (res) => {
-                    console.log(`onClientUploadComplete`, res);
-                }}
-                onUploadBegin={() => {
-                    console.log("upload begin");
-                }}
-            />
-            <UploadDropzone
-                /**
-                 * @see https://docs.uploadthing.com/api-reference/react#uploaddropzone
-                 */
-                endpoint="videoAndImage"
-                onClientUploadComplete={(res) => {
-                    console.log(`onClientUploadComplete`, res);
-                }}
-                onUploadBegin={() => {
-                    console.log("upload begin");
-                }}
-            />
-            <h1>Your files</h1>
-            <div>
-                {isLoading ? (
-                    <div>Waiting for files</div>
-                ) : data?.files.length ? (
-                    data?.files.map((file) => (
-                        <div style={{ display: "flex", gap: 8 }}>
-                            <div>Name: {file.name}</div>
-                            <a href={file.url} target="_blank">
-                                View
-                            </a>
-                        </div>
-                    ))
-                ) : (
-                    <i>No files uploaded yet</i>
-                )}
+  return (
+    <main
+      className={inter.className}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <UploadButton
+        /**
+         * @see https://docs.uploadthing.com/api-reference/react#uploadbutton
+         */
+        endpoint="videoAndImage"
+        onClientUploadComplete={async (res) => {
+          console.log(`onClientUploadComplete`, res);
+        }}
+        onUploadBegin={() => {
+          console.log("upload begin");
+        }}
+      />
+      <UploadDropzone
+        /**
+         * @see https://docs.uploadthing.com/api-reference/react#uploaddropzone
+         */
+        endpoint="videoAndImage"
+        onClientUploadComplete={(res) => {
+          console.log(`onClientUploadComplete`, res);
+        }}
+        onUploadBegin={() => {
+          console.log("upload begin");
+        }}
+      />
+      <h1>Your files</h1>
+      <div>
+        {isLoading ? (
+          <div>Waiting for files</div>
+        ) : data?.files.length ? (
+          data?.files.map((file) => (
+            <div style={{ display: "flex", gap: 8 }}>
+              <div>Name: {file.name}</div>
+              <a href={file.url} target="_blank">
+                View
+              </a>
             </div>
-        </main>
-    );
+          ))
+        ) : (
+          <i>No files uploaded yet</i>
+        )}
+      </div>
+    </main>
+  );
 }
