260002e497bf85c89306850f8b2e92072592d1f1
juliusmarminge <julius0216@outlook.com>
2023-09-15 09:39:23 +0200

    fix

--- examples/with-clerk-pagesdir/src/pages/_app.tsx
+++ examples/with-clerk-pagesdir/src/pages/_app.tsx
@@ -4,16 +4,16 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "@uploadthing/react/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <ClerkProvider {...pageProps}>
-            <Component {...pageProps} />
-        </ClerkProvider>
-    );
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
 
 // Opt out of prerendering
 MyApp.getInitialProps = async () => {
-    return {};
-}
+  return {};
+};
 
 export default MyApp;
