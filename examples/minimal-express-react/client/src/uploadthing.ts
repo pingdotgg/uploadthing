9fdc9903a57e5bb113f2f689f6ce2a9919924db5
juliusmarminge <julius0216@outlook.com>
2023-09-15 11:07:57 +0200

    src

--- examples/minimal-express-react/client/src/uploadthing.ts
+++ examples/minimal-express-react/client/src/uploadthing.ts
@@ -1,7 +1,7 @@
 import { generateComponents } from "@uploadthing/react";
 import { generateReactHelpers } from "@uploadthing/react/hooks";
 
-import type { OurFileRouter } from "../../server";
+import type { OurFileRouter } from "../../server/src/router";
 
 export const { UploadButton, UploadDropzone, Uploader } =
   generateComponents<OurFileRouter>();
