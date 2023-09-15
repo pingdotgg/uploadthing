139382a3bf4a58567b3b1f717df034f4d37aa603
juliusmarminge <julius0216@outlook.com>
2023-09-15 12:04:17 +0200

    use default port, set NDOE_ENV

--- examples/minimal-fastify-react/server/src/index.ts
+++ examples/minimal-fastify-react/server/src/index.ts
@@ -20,7 +20,7 @@ fastify
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3001 });
+    await fastify.listen({ port: 3000 });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
