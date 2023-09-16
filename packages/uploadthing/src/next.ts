29ea53ceba48d396035e2c55550374b90aa97f92
juliusmarminge <julius0216@outlook.com>
2023-09-17 00:03:18 +0200

    plain alias

--- packages/uploadthing/src/next.ts
+++ packages/uploadthing/src/next.ts
@@ -1,9 +1,5 @@
-import type { NextRequest } from "next/server";
-
 import type { Json } from "@uploadthing/shared";
 
-import type { RouterWithConfig } from "./internal/handler";
-import type { FileRouter } from "./internal/types";
 import type { CreateBuilderOptions } from "./internal/upload-builder";
 import { createBuilder } from "./internal/upload-builder";
 import { createServerHandler } from "./server";
@@ -14,18 +10,4 @@ export const createUploadthing = <TErrorShape extends Json>(
   opts?: CreateBuilderOptions<TErrorShape>,
 ) => createBuilder<"app", TErrorShape>(opts);
 
-export const createNextRouteHandler = <TRouter extends FileRouter>(
-  opts: RouterWithConfig<TRouter>,
-) => {
-  const handlers = createServerHandler(opts);
-
-  const POST = async (req: NextRequest) => {
-    return handlers.POST(req);
-  };
-
-  const GET = (req: NextRequest) => {
-    return handlers.GET(req);
-  };
-
-  return { GET, POST };
-};
+export const createNextRouteHandler = createServerHandler;
