a06304f760cfc601a136aa151537ee43398197a9
juliusmarminge <julius0216@outlook.com>
2023-09-17 00:12:35 +0200

    revert

--- packages/uploadthing/src/next.ts
+++ packages/uploadthing/src/next.ts
@@ -1,7 +1,11 @@
+import type { NextRequest } from "next/server";
+
 import type { Json } from "@uploadthing/shared";
 
+import type { RouterWithConfig } from "./internal/handler";
 import type { CreateBuilderOptions } from "./internal/upload-builder";
 import { createBuilder } from "./internal/upload-builder";
+import type { FileRouter } from "./server";
 import { createServerHandler } from "./server";
 
 export type { FileRouter } from "./internal/types";
@@ -10,4 +14,13 @@ export const createUploadthing = <TErrorShape extends Json>(
   opts?: CreateBuilderOptions<TErrorShape>,
 ) => createBuilder<"app", TErrorShape>(opts);
 
-export const createNextRouteHandler = createServerHandler;
+export const createNextRouteHandler = <TRouter extends FileRouter>(
+  opts: RouterWithConfig<TRouter>,
+) => {
+  const handlers = createServerHandler(opts);
+
+  return {
+    POST: (req: NextRequest) => handlers.POST(req),
+    GET: (req: NextRequest) => handlers.GET(req),
+  };
+};
