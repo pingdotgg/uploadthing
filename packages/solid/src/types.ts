5e8016b32fc7709dcd855da33dbc2ecf18eac0b5
Mr0Bread <55754540+Mr0Bread@users.noreply.github.com>
2023-09-18 10:51:18 +0300

    feat: Solid custom styling and content (#298)
    Co-authored-by: Vladislav Zimnikov <vladislavzimnikov@Vladislavs-MacBook-Pro.local>
    Co-authored-by: juliusmarminge <julius0216@outlook.com>

---
+++ packages/solid/src/types.ts
@@ -0,0 +1,20 @@
+import type { UploadFileResponse } from "uploadthing/client";
+import type { FileRouter, inferEndpointInput } from "uploadthing/server";
+
+export type UploadthingComponentProps<TRouter extends FileRouter> = {
+  [TEndpoint in keyof TRouter]: {
+    endpoint: TEndpoint;
+
+    onUploadProgress?: (progress: number) => void;
+    onClientUploadComplete?: (res?: UploadFileResponse[]) => void;
+    onUploadError?: (error: Error) => void;
+    onUploadBegin?: (fileName: string) => void;
+    url?: string;
+    multiple?: boolean;
+  } & (undefined extends inferEndpointInput<TRouter[TEndpoint]>
+    ? // eslint-disable-next-line @typescript-eslint/ban-types
+      {}
+    : {
+        input: inferEndpointInput<TRouter[TEndpoint]>;
+      });
+}[keyof TRouter];
---
+++ packages/uploadthing/src/internal/component-theming.ts
@@ -0,0 +1,139 @@
+import type { CSSProperties, ReactNode } from "react";
+import type { JSX } from "solid-js/jsx-runtime";
+
+import type { ExpandedRouteConfig } from "@uploadthing/shared";
+
+/**
+ * Shared helpers for our premade components that's reusable by multiple frameworks
+ */
+
+export const generatePermittedFileTypes = (config?: ExpandedRouteConfig) => {
+  const fileTypes = config ? Object.keys(config) : [];
+
+  const maxFileCount = config
+    ? Object.values(config).map((v) => v.maxFileCount)
+    : [];
+
+  return { fileTypes, multiple: maxFileCount.some((v) => v && v > 1) };
+};
+
+export const capitalizeStart = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const INTERNAL_doFormatting = (config?: ExpandedRouteConfig): string => {
+  if (!config) return "";
+
+  const allowedTypes = Object.keys(config) as (keyof ExpandedRouteConfig)[];
+
+  const formattedTypes = allowedTypes.map((f) => (f === "blob" ? "file" : f));
+
+  // Format multi-type uploader label as "Supports videos, images and files";
+  if (formattedTypes.length > 1) {
+    const lastType = formattedTypes.pop();
+    return `${formattedTypes.join("s, ")} and ${lastType}s`;
+  }
+
+  // Single type uploader label
+  const key = allowedTypes[0];
+  const formattedKey = formattedTypes[0];
+
+  const { maxFileSize, maxFileCount } = config[key]!;
+
+  if (maxFileCount && maxFileCount > 1) {
+    return `${formattedKey}s up to ${maxFileSize}, max ${maxFileCount}`;
+  } else {
+    return `${formattedKey} (${maxFileSize})`;
+  }
+};
+
+export const allowedContentTextLabelGenerator = (
+  config?: ExpandedRouteConfig,
+): string => {
+  return capitalizeStart(INTERNAL_doFormatting(config));
+};
+
+export const progressWidths: Record<number, string> = {
+  0: "after:w-0",
+  10: "after:w-[10%]",
+  20: "after:w-[20%]",
+  30: "after:w-[30%]",
+  40: "after:w-[40%]",
+  50: "after:w-[50%]",
+  60: "after:w-[60%]",
+  70: "after:w-[70%]",
+  80: "after:w-[80%]",
+  90: "after:w-[90%]",
+  100: "after:w-[100%]",
+};
+
+type AnyRuntime = "react" | "solid";
+type MinCallbackArg = { __runtime: AnyRuntime };
+type inferRuntime<T extends MinCallbackArg> = T["__runtime"] extends "react"
+  ? "react"
+  : "solid";
+
+type ElementEsque<TRuntime extends AnyRuntime> = TRuntime extends "react"
+  ? ReactNode
+  : JSX.Element;
+type CSSPropertiesEsque<TRuntime extends AnyRuntime> = TRuntime extends "react"
+  ? CSSProperties
+  : JSX.CSSProperties;
+
+export type StyleField<
+  CallbackArg extends MinCallbackArg,
+  TRuntime extends AnyRuntime = inferRuntime<CallbackArg>,
+> =
+  | string
+  | CSSPropertiesEsque<TRuntime>
+  | ((
+      arg: Omit<CallbackArg, "__runtime">,
+    ) => string | CSSPropertiesEsque<TRuntime>);
+
+export type ContentField<
+  CallbackArg extends MinCallbackArg,
+  TRuntime extends AnyRuntime = inferRuntime<CallbackArg>,
+> =
+  | ElementEsque<TRuntime>
+  | ((arg: Omit<CallbackArg, "__runtime">) => ElementEsque<TRuntime>);
+
+export const styleFieldToClassName = <T extends MinCallbackArg>(
+  styleField: StyleField<T> | undefined,
+  args: T,
+) => {
+  if (typeof styleField === "string") return styleField;
+  if (typeof styleField === "function") {
+    const result = styleField(args);
+
+    if (typeof result === "string") return result;
+  }
+
+  return "";
+};
+
+export const styleFieldToCssObject = <T extends MinCallbackArg>(
+  styleField: StyleField<T> | undefined,
+  args: T,
+) => {
+  if (typeof styleField === "object") return styleField;
+  if (typeof styleField === "function") {
+    const result = styleField(args);
+
+    if (typeof result === "object") return result;
+  }
+
+  return {};
+};
+
+export const contentFieldToContent = <T extends MinCallbackArg>(
+  contentField: ContentField<T> | undefined,
+  arg: T,
+) => {
+  if (!contentField) return null;
+  if (typeof contentField !== "function") return contentField;
+  if (typeof contentField === "function") {
+    const result = contentField(arg);
+
+    return result;
+  }
+};
