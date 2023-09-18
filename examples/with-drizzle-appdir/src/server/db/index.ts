fe00a848f0030a3f70cf6ccadd4b593de623ff95
juliusmarminge <julius0216@outlook.com>
2023-09-18 09:38:20 +0200

    noice

--- examples/with-drizzle-appdir/src/server/db/index.ts
+++ examples/with-drizzle-appdir/src/server/db/index.ts
@@ -1,5 +1,5 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { Database } from "bun:sqlite";
+import { drizzle } from "drizzle-orm/bun-sqlite";
 
 import * as schema from "./schema";
 
