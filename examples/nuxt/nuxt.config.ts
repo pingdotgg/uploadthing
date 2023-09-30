a0d0b0828051abf82a2b0cd3371c2b999b497124
juliusmarminge <julius0216@outlook.com>
2023-09-30 18:40:33 +0200

    move exmaple to module playground

--- examples/nuxt/nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  typescript: { typeCheck: true },
-  // modules: ["nuxt-uploadthing"],
-});
