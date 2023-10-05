98ccec491dcc6b5e59c505a5afbbb6b9455aba64
juliusmarminge <julius0216@outlook.com>
2023-10-05 21:25:27 +0200

    revert example, add docs

--- examples/minimal-appdir/src/server/uploadthing.ts
+++ examples/minimal-appdir/src/server/uploadthing.ts
@@ -12,7 +12,6 @@ export const uploadRouter = {
     image: {
       maxFileSize: "4MB",
       maxFileCount: 4,
-      contentDisposition: "attachment",
     },
     video: {
       maxFileSize: "16MB",
