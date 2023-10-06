9aed3eaa317a3320a6418231ba27c0bde0323879
juliusmarminge <julius0216@outlook.com>
2023-10-06 17:05:46 +0200

    fix solid port

--- examples/minimal-solidstart/src/utils/uploadthing.ts
+++ examples/minimal-solidstart/src/utils/uploadthing.ts
@@ -3,6 +3,8 @@ import { generateComponents, generateSolidHelpers } from "@uploadthing/solid";
 import type { OurFileRouter } from "~/server/uploadthing";
 
 export const { UploadButton, UploadDropzone } =
-  generateComponents<OurFileRouter>("http://localhost:9898");
+  generateComponents<OurFileRouter>({
+    url: "http://localhost:9898/api/uploadthing",
+  });
 
 export const { useUploadThing } = generateSolidHelpers<OurFileRouter>();
