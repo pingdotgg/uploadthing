623bcfbb699934cffbaefd1de9329b715e1e7e16
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-10-12 13:24:50 -0700

    refactor withExponentialBackoff to allow setting either time based or retry count based limit

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -123,31 +123,70 @@ export function generateUploadThingURL(path: `/${string}`) {
 
 export const withExponentialBackoff = async <T>(
   doTheThing: () => Promise<T | null>,
-  MAXIMUM_BACKOFF_MS = 64 * 1000,
-  MAX_RETRIES = 20,
+  config: {
+    maxBackoffMs?: number;
+    maxRetries?: number;
+    maxTotalTimeMs?: number;
+  } = {
+    maxRetries: 20,
+  },
 ): Promise<T | null> => {
-  let tries = 0;
-  let backoffMs = 500;
-  let backoffFuzzMs = 0;
+  if (!config.maxBackoffMs) config.maxBackoffMs = 64 * 1000;
 
-  let result = null;
-  while (tries <= MAX_RETRIES) {
-    result = await doTheThing();
-    if (result !== null) return result;
+  if (config.maxTotalTimeMs) {
+    // retry until we hit the max total time
 
-    tries += 1;
-    backoffMs = Math.min(MAXIMUM_BACKOFF_MS, backoffMs * 2);
-    backoffFuzzMs = Math.floor(Math.random() * 500);
+    let elapsedMs = 0;
+    let backoffMs = 500;
+    let backoffFuzzMs = 0;
 
-    if (tries > 3) {
-      console.error(
-        `[UT] Call unsuccessful after ${tries} tries. Retrying in ${Math.floor(
-          backoffMs / 1000,
-        )} seconds...`,
-      );
+    let result = null;
+
+    while (elapsedMs <= config.maxTotalTimeMs) {
+      result = await doTheThing();
+      if (result !== null) return result;
+
+      elapsedMs += backoffMs + backoffFuzzMs;
+      backoffMs = Math.min(config.maxBackoffMs, backoffMs * 2);
+      backoffFuzzMs = Math.floor(Math.random() * 500);
+
+      if (elapsedMs > 3000) {
+        console.error(
+          `[UT] Call unsuccessful after ${Math.floor(
+            elapsedMs / 1000,
+          )} seconds. Retrying in ${Math.floor(backoffMs / 1000)} seconds...`,
+        );
+      }
+
+      await new Promise((r) => setTimeout(r, backoffMs + backoffFuzzMs));
     }
+  } else {
+    // retry until we hit the max retries
 
-    await new Promise((r) => setTimeout(r, backoffMs + backoffFuzzMs));
+    let tries = 0;
+    let backoffMs = 500;
+    let backoffFuzzMs = 0;
+
+    let result = null;
+
+    while (tries <= config.maxRetries!) {
+      result = await doTheThing();
+      if (result !== null) return result;
+
+      tries += 1;
+      backoffMs = Math.min(config.maxBackoffMs, backoffMs * 2);
+      backoffFuzzMs = Math.floor(Math.random() * 500);
+
+      if (tries > 3) {
+        console.error(
+          `[UT] Call unsuccessful after ${tries} tries. Retrying in ${Math.floor(
+            backoffMs / 1000,
+          )} seconds...`,
+        );
+      }
+
+      await new Promise((r) => setTimeout(r, backoffMs + backoffFuzzMs));
+    }
   }
 
   return null;
@@ -156,25 +195,31 @@ export const withExponentialBackoff = async <T>(
 export async function pollForFileData(
   fileKey: string,
   callback?: (json: any) => Promise<any>,
+  maxTimeoutMs?: number,
 ) {
   const queryUrl = generateUploadThingURL(`/api/pollUpload/${fileKey}`);
 
-  return withExponentialBackoff(async () => {
-    const res = await fetch(queryUrl);
-    const maybeJson = await safeParseJSON<
-      { status: "done"; fileData: FileData } | { status: "something else" }
-    >(res);
+  return withExponentialBackoff(
+    async () => {
+      const res = await fetch(queryUrl);
+      const maybeJson = await safeParseJSON<
+        { status: "done"; fileData: FileData } | { status: "something else" }
+      >(res);
 
-    if (maybeJson instanceof Error) {
-      console.error(
-        `[UT] Error polling for file data for ${fileKey}: ${maybeJson.message}`,
-      );
-      return null;
-    }
+      if (maybeJson instanceof Error) {
+        console.error(
+          `[UT] Error polling for file data for ${fileKey}: ${maybeJson.message}`,
+        );
+        return null;
+      }
 
-    if (maybeJson.status !== "done") return null;
-    await callback?.(maybeJson);
-  });
+      if (maybeJson.status !== "done") return null;
+      await callback?.(maybeJson);
+    },
+    {
+      maxTotalTimeMs: maxTimeoutMs,
+    },
+  );
 }
 
 export function getUploadthingUrl() {
