6ccae14ac620381a22bca51c22527d1652bedd4d
Theo Browne <me@t3.gg>
2023-10-24 21:46:42 -0700

    Remove zod from shared

--- packages/shared/src/file-types.ts
+++ packages/shared/src/file-types.ts
@@ -1,9 +1,3 @@
-import * as z from "zod";
-
-import { mimeTypes } from "@uploadthing/mime-types/db";
-
-import { objectKeys } from "./utils";
-
 export const ALLOWED_FILE_TYPES = [
   "image",
   "video",
@@ -14,20 +8,3 @@ export const ALLOWED_FILE_TYPES = [
 ] as const;
 
 export type AllowedFileType = (typeof ALLOWED_FILE_TYPES)[number];
-
-function zodEnumFromObjKeys<K extends string>(
-  obj: Record<K, any>,
-): z.ZodEnum<[K, ...K[]]> {
-  const [firstKey, ...otherKeys] = objectKeys(obj);
-  return z.enum([firstKey, ...otherKeys]);
-}
-
-export const MimeTypeZod = zodEnumFromObjKeys(mimeTypes);
-
-export const InternalFileTypeValidator = z.enum(ALLOWED_FILE_TYPES);
-export const InternalMimeTypeValidator = MimeTypeZod;
-
-export const CombinedTypeValidator = z.union([
-  InternalFileTypeValidator,
-  InternalMimeTypeValidator,
-]);
