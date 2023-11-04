6ba5f38c905157401054c43f2a9de9c4eb28af1d
juliusmarminge <julius0216@outlook.com>
2023-11-04 16:04:53 +0100

    fmt

--- packages/react/src/components/dropzone.tsx
+++ packages/react/src/components/dropzone.tsx
@@ -107,6 +107,7 @@ export function UploadDropzone<TRouter extends FileRouter>(
   const onDrop = useCallback(
     (acceptedFiles: FileWithPath[]) => {
       setFiles(acceptedFiles);
+
       // If mode is auto, start upload immediately
       if (mode === "auto") {
         const input = "input" in $props ? $props.input : undefined;
