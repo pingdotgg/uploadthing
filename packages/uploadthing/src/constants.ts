36c2b2883d22cc232d9391214ca2b2187d834156
juliusmarminge <julius0216@outlook.com>
2023-11-07 08:17:08 +0100

    resolve json module

--- packages/uploadthing/src/constants.ts
+++ packages/uploadthing/src/constants.ts
@@ -1,6 +1,3 @@
-import type { PackageJson } from "type-fest";
+import { version } from "../package.json";
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require("../package.json") as PackageJson;
-if (!packageJson.version) throw new Error("no version found in package.json");
-export const UPLOADTHING_VERSION = packageJson.version;
+export const UPLOADTHING_VERSION = version;
