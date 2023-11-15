618b031c2ce32b6cb01c179be50a1236a8992ab4
juliusmarminge <julius0216@outlook.com>
2023-11-15 11:15:31 +0100

    rev

--- packages/uploadthing/src/internal/parser.ts
+++ packages/uploadthing/src/internal/parser.ts
@@ -4,14 +4,14 @@ import type { MaybePromise } from "./types";
 
 // Don't want to use Zod cause it's an optional dependency
 export type ParseFn<TType> = (input: unknown) => MaybePromise<TType>;
-export type ParserZodEsque<TInput, TParsedInput extends Json | undefined> = {
+export type ParserZodEsque<TInput, TParsedInput extends Json> = {
   _input: TInput;
   _output: TParsedInput; // if using .transform etc
   parse: ParseFn<TParsedInput>;
 };
 
 // In case we add support for more parsers later
-export type JsonParser = ParserZodEsque<Json | undefined, Json | undefined>;
+export type JsonParser = ParserZodEsque<Json, Json>;
 
 export function getParseFn<TParser extends JsonParser>(
   parser: TParser,
