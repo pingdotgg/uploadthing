09197e0b7c2091aa413c81f2c69490afdf8c2f26
Theo Browne <me@t3.gg>
2023-11-25 18:43:11 -0800

    remove all examples and make sandbox for dev

--- examples/with-clerk-pagesdir/src/pages/api/uploadthing.ts
+++
@@ -1,9 +0,0 @@
-import { createNextPageApiHandler } from "uploadthing/next-legacy";
-
-import { uploadRouter } from "~/server/uploadthing";
-
-const handler = createNextPageApiHandler({
-  router: uploadRouter,
-});
-
-export default handler;
--- examples/with-drizzle-appdir/src/server/db/index.ts
+++
@@ -1,7 +0,0 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-
-import * as schema from "./schema";
-
-const sqlite = new Database(".data/sqlite.db");
-export const db = drizzle(sqlite, { schema });
