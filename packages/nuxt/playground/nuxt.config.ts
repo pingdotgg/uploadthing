d0c5cf4309db045a3ed9400a017cc2292bf0f950
juliusmarminge <julius0216@outlook.com>
2024-01-20 18:00:14 +0100

    rm css from user config

--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -1,7 +1,6 @@
 export default defineNuxtConfig({
   modules: ["../src/module"],
   devtools: { enabled: true },
-  css: ["@uploadthing/vue/styles.css"],
   uploadthing: {
     logLevel: "debug",
   },
