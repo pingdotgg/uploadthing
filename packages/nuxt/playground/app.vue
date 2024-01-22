45247dc24cc2f81480c1ccbc7c9811a252e90adc
juliusmarminge <julius0216@outlook.com>
2024-01-22 16:11:30 +0100

    fix type

--- packages/nuxt/playground/app.vue
+++ packages/nuxt/playground/app.vue
@@ -37,7 +37,7 @@
     @change="
       async (e) => {
         console.log(`e`, e);
-        const file = e.target.files[0];
+        const file = (e.target as HTMLInputElement).files?.[0];
         if (!file) return;
 
         // Do something with files
