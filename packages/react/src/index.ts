72ce72474a23837e6e913d9297db45c489750dff
juliusmarminge <julius0216@outlook.com>
2024-01-24 11:15:12 +0100

    export hooks from main entrypoint

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -10,5 +10,9 @@ export {
   generateComponents,
 } from "./components";
 
+export { generateReactHelpers } from "./useUploadThing";
+
 export type * from "./types";
+
+export { useDropzone } from "@uploadthing/dropzone/react";
 export type * from "@uploadthing/dropzone/react";
