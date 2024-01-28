b4243fd8d50bfaeb1a811c328c32448f4714a17b
juliusmarminge <julius0216@outlook.com>
2024-01-28 23:20:49 +0100

    resolve transistive dep

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {},
   resolve: {
     alias: {
+      "@uploadthing/mime-types/db": join(__dirname, "../mime-types/src/db"),
       "uploadthing/server": join(__dirname, "../uploadthing/src/server"),
       "uploadthing/client": join(__dirname, "../uploadthing/src/client"),
     },
