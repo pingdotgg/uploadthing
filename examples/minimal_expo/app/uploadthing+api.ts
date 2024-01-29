ee7fd879a244462fdd9a19c2fdd3dc8b73dca884
juliusmarminge <julius0216@outlook.com>
2024-01-29 09:37:28 +0100

    re-init expo app

---
+++ examples/minimal_expo/app/uploadthing+api.ts
@@ -0,0 +1,41 @@
+import { createRouteHandler, createUploadthing } from "uploadthing/server";
+import type { FileRouter } from "uploadthing/server";
+
+const f = createUploadthing();
+
+const uploadRouter = {
+  videoAndImage: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 4,
+    },
+    video: {
+      maxFileSize: "16MB",
+    },
+  })
+    .middleware(({ req }) => {
+      // Check some condition based on the incoming requrest
+      req;
+      //^?
+      // if (!req.headers["x-some-header"]) {
+      //   throw new Error("x-some-header is required");
+      // }
+
+      // Return some metadata to be stored with the file
+      return { foo: "bar" as const };
+    })
+    .onUploadComplete(({ file, metadata }) => {
+      metadata;
+      // ^?
+      console.log("upload completed", file);
+    }),
+} satisfies FileRouter;
+
+export type UploadRouter = typeof uploadRouter;
+
+export const { GET, POST } = createRouteHandler({
+  router: uploadRouter,
+  config: {
+    uploadthingSecret: "sk_live_xxx",
+  },
+});
