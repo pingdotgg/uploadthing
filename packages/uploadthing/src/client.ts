432bfcf09a58d08988fdb37e9a08fb5f3018f236
Mark R. Florkowski <mark.florkowski@gmail.com>
2024-01-30 12:21:39 -0800

    misc cleanup

--- packages/uploadthing/src/client.ts
+++ packages/uploadthing/src/client.ts
@@ -1,17 +1,13 @@
-// Don't want to ship our logger to the client, keep size down
-/* eslint-disable no-console */
+/* eslint-disable no-console -- Don't ship our logger to client, reduce size*/
 
+import { Effect, Cause } from "effect";
 import { Schema } from "@effect/schema";
-import { Cause, Duration, Effect, pipe, Schedule } from "effect";
-
 import type { ResponseEsque } from "@uploadthing/shared";
 import {
   safeParseJSON,
   UploadThingError,
-  withExponentialBackoff,
 } from "@uploadthing/shared";
 
-import { UPLOADTHING_VERSION } from "./constants";
 import { exponentialBackoff10Sec, fetchEff, fetchEffJson } from "./effect-utils";
 import { resolveMaybeUrlArg } from "./internal/get-full-api-url";
 import type { UploadThingResponse } from "./internal/handler";
@@ -101,8 +97,8 @@ const DANGEROUS__uploadFiles_internal = <
 >(
   endpoint: TEndpoint,
   opts: UploadFilesOptions<TRouter, TEndpoint>,
-) =>
-  Effect.gen(function* ($) {
+) => {
+  return Effect.gen(function* ($) {
     // Fine to use global fetch in browser
     const fetch = globalThis.fetch.bind(globalThis);
     const reportEventToUT = createUTReporter({
@@ -139,42 +135,30 @@ const DANGEROUS__uploadFiles_internal = <
       const error = yield* $(
         Effect.promise(() => UploadThingError.fromResponse(s3ConnectionRes_)),
       );
-
       return yield* $(error);
     }
 
-    const jsonOrError = yield* $(
+    const s3ConnectionRes = yield* $(
       safeParseJSONEff<UploadThingResponse>(s3ConnectionRes_),
     );
-    if (jsonOrError instanceof Error) {
+
+    if (s3ConnectionRes instanceof Error) {
       return yield* $(
         new UploadThingError({
           code: "BAD_REQUEST",
-          message: jsonOrError.message,
+          message: s3ConnectionRes.message,
           cause: s3ConnectionRes_,
         }),
       );
     }
 
-    const s3ConnectionRes = jsonOrError;
-
-    // TODO is this still needed?
-    // if (!s3ConnectionRes || !Array.isArray(s3ConnectionRes)) {
-    //   throw new UploadThingError({
-    //     code: "BAD_REQUEST",
-    //     message: "No URL. How did you even get here?",
-    //     cause: s3ConnectionRes,
-    //   });
-    // }
-
     return yield* $(
       Effect.all(
-        s3ConnectionRes.map((presigned) =>
-          uploadFile(opts, presigned, reportEventToUT),
-        ),
+        s3ConnectionRes.map((d) => uploadFile(opts, d, reportEventToUT)),
       ),
     );
   });
+}
 
 type ReportEventToUT = <TEvent extends keyof UTEvents>(
   type: TEvent,
@@ -215,8 +199,6 @@ const uploadFile = <
     } = presigned;
 
     let uploadedBytes = 0;
-
-    // let etags: { tag: string; partNumber: number }[];
 
     const innerEffect = (url: string, index: number) =>
       Effect.gen(function* ($) {
@@ -273,17 +255,8 @@ const uploadFile = <
       );
     }
 
-    // // Poll for file data, this way we know that the client-side onUploadComplete callback will be called after the server-side version
-    // yield* $(
-    //   Effect.promise(() =>
-    //     pollForFileData({
-    //       url: pollingUrl,
-    //       apiKey: null,
-    //       sdkVersion: UPLOADTHING_VERSION,
-    //     }),
-    //   ),
-    // );
     // wait a bit as it's unsreasonable to expect the server to be done by now
+    // TODO: This should probably be responsive to the size of the file (?)
     yield* $(Effect.sleep(1000));
 
     const PollingResponse = Schema.union(
@@ -298,6 +271,8 @@ const uploadFile = <
     );
 
     const serverData = yield* $(
+      // TODO: Figure out this lint error
+      // eslint-disable-next-line no-restricted-globals
       fetchEffJson(fetch, PollingResponse, pollingUrl, {
         headers: { authorization: pollingJwt },
       }),
@@ -335,150 +310,6 @@ export const DANGEROUS__uploadFiles = <
     Effect.tapErrorCause(Effect.logError),
     Effect.runPromise,
   );
-
-// export const DANGEROUS__uploadFiles_old = async <TRouter extends FileRouter>(
-//   opts: UploadFilesOptions<TRouter>,
-// ) => {
-//   const reportEventToUT = createUTReporter({
-//     endpoint: String(opts.endpoint),
-//     url: opts?.url,
-//   });
-
-//   // Get presigned URL for S3 upload
-//   const s3ConnectionRes = await fetch(
-//     createAPIRequestUrl({
-//       url: opts.url,
-//       slug: String(opts.endpoint),
-//       actionType: "upload",
-//     }),
-//     {
-//       method: "POST",
-//       body: JSON.stringify({
-//         files: opts.files.map((f) => ({ name: f.name, size: f.size })),
-//         input: opts.input,
-//       }),
-//       // Express requires Content-Type to be explicitly set to parse body properly
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     },
-//   ).then(async (res) => {
-//     // check for 200 response
-//     if (!res.ok) {
-//       const error = await UploadThingError.fromResponse(res);
-//       throw error;
-//     }
-
-//     const jsonOrError = await safeParseJSON<UploadThingResponse>(res);
-//     if (jsonOrError instanceof Error) {
-//       throw new UploadThingError({
-//         code: "BAD_REQUEST",
-//         message: jsonOrError.message,
-//         cause: res,
-//       });
-//     }
-//     return jsonOrError;
-//   });
-
-//   if (!s3ConnectionRes || !Array.isArray(s3ConnectionRes)) {
-//     throw new UploadThingError({
-//       code: "BAD_REQUEST",
-//       message: "No URL. How did you even get here?",
-//       cause: s3ConnectionRes,
-//     });
-//   }
-
-//   const fileUploadPromises = s3ConnectionRes.map(async (presigned) => {
-//     const file = opts.files.find((f) => f.name === presigned.fileName);
-
-//     if (!file) {
-//       console.error("No file found for presigned URL", presigned);
-//       throw new UploadThingError({
-//         code: "NOT_FOUND",
-//         message: "No file found for presigned URL",
-//         cause: `Expected file with name ${
-//           presigned.fileName
-//         } but got '${opts.files.join(",")}'`,
-//       });
-//     }
-
-//     const {
-//       presignedUrls,
-//       uploadId,
-//       chunkSize,
-//       contentDisposition,
-//       key,
-//       pollingUrl,
-//     } = presigned;
-
-//     let uploadedBytes = 0;
-
-//     let etags: { tag: string; partNumber: number }[];
-//     try {
-//       etags = await Promise.all(
-//         presignedUrls.map(async (url, index) => {
-//           const offset = chunkSize * index;
-//           const end = Math.min(offset + chunkSize, file.size);
-//           const chunk = file.slice(offset, end);
-
-//           const etag = await uploadPartWithProgress({
-//             url,
-//             chunk: chunk,
-//             contentDisposition,
-//             fileType: file.type,
-//             fileName: file.name,
-//             maxRetries: 10,
-//             onProgress: (delta) => {
-//               uploadedBytes += delta;
-//               const percent = (uploadedBytes / file.size) * 100;
-//               opts.onUploadProgress?.({ file: file.name, progress: percent });
-//             },
-//           });
-
-//           return { tag: etag, partNumber: index + 1 };
-//         }),
-//       );
-//     } catch (error) {
-//       await reportEventToUT("failure", {
-//         fileKey: key,
-//         uploadId,
-//         fileName: file.name,
-//         s3Error: (error as Error).toString(),
-//       });
-//       throw "unreachable"; // failure event will throw for us
-//     }
-
-//     // Tell the server that the upload is complete
-//     const uploadOk = await reportEventToUT("multipart-complete", {
-//       uploadId,
-//       fileKey: key,
-//       etags,
-//     });
-//     if (!uploadOk) {
-//       console.log("Failed to alert UT of upload completion");
-//       throw new UploadThingError({
-//         code: "UPLOAD_FAILED",
-//         message: "Failed to alert UT of upload completion",
-//       });
-//     }
-
-//     // Poll for file data, this way we know that the client-side onUploadComplete callback will be called after the server-side version
-//     await pollForFileData({
-//       url: pollingUrl,
-//       apiKey: null,
-//       sdkVersion: UPLOADTHING_VERSION,
-//     });
-
-//     return {
-//       name: file.name,
-//       size: file.size,
-//       key: presigned.key,
-//       url: "https://utfs.io/f/" + key,
-//     } satisfies UploadFileResponse;
-//   });
-
-//   return Promise.all(fileUploadPromises);
-// };
 
 export const genUploader = <TRouter extends FileRouter>(initOpts: {
   /**
@@ -544,42 +375,6 @@ export const generateClientDropzoneAccept = (fileTypes: string[]) => {
 
   return Object.fromEntries(mimeTypes.map((type) => [type, []]));
 };
-
-// Returns a full URL to the dev's uploadthing endpoint
-export function getFullApiUrl(maybeUrl?: string): URL {
-  const base = (() => {
-    if (typeof window !== "undefined") {
-      return window.location.origin;
-    }
-
-    // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
-    if (typeof process !== "undefined" && process?.env?.VERCEL_URL) {
-      return `https://${process.env.VERCEL_URL}`;
-    }
-
-    // @ts-expect-error - import meta is not defined in node
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    if (import.meta.env?.VERCEL_URL) {
-      // @ts-expect-error - import meta is not defined in node
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      return `https://${import.meta.env.VERCEL_URL}`;
-    }
-
-    return "http://localhost:3000";
-  })();
-
-  try {
-    const url = new URL(maybeUrl ?? "/api/uploadthing", base);
-    if (url.pathname === "/") {
-      url.pathname = "/api/uploadthing";
-    }
-    return url;
-  } catch (err) {
-    throw new Error(
-      `Failed to parse '${maybeUrl}' as a URL. Make sure it's a valid URL or path`,
-    );
-  }
-}
 
 const safeParseJSONEff = <T>(res: ResponseEsque) =>
   Effect.promise<T | Error>(() => safeParseJSON<T>(res));
