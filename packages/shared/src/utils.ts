c1dbd6dbe3df725284415d0bd0402b308263a5b1
juliusmarminge <julius0216@outlook.com>
2024-02-07 10:03:08 +0100

    more paths

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -247,3 +247,5 @@ export function objectKeys<T extends Record<string, unknown>>(
 export function isObject(obj: unknown): obj is Record<string, unknown> {
   return typeof obj === "object" && obj !== null && !Array.isArray(obj);
 }
+
+export const A = "a";
