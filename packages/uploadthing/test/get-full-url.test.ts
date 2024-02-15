ff38410f97ba18743a27e2604ff42cdf7bce4fe7
juliusmarminge <julius0216@outlook.com>
2024-02-15 23:11:34 +0100

    fix test

--- packages/uploadthing/test/get-full-url.test.ts
+++ packages/uploadthing/test/get-full-url.test.ts
@@ -1,3 +1,4 @@
+import { Effect } from "effect";
 import { process } from "std-env";
 import { describe, expect, it } from "vitest";
 
@@ -8,7 +9,7 @@ import { resolveMaybeUrlArg } from "../src/internal/get-full-api-url";
 describe("getFullApiUrl", () => {
   it("should return the provided url if it is already absolute", () => {
     const url = "https://example.com/foo/bar";
-    expect(resolveMaybeUrlArg(url).href).toBe(url);
+    expect(Effect.runSync(resolveMaybeUrlArg(url)).href).toBe(url);
   });
 
   it("should add `window.location.origin` if the url is relative and request is clientside", () => {
@@ -19,7 +20,7 @@ describe("getFullApiUrl", () => {
       },
     } as any;
 
-    expect(resolveMaybeUrlArg("/foo/bar").href).toBe(
+    expect(Effect.runSync(resolveMaybeUrlArg("/foo/bar")).href).toBe(
       "http://example.com/foo/bar",
     );
 
@@ -30,7 +31,7 @@ describe("getFullApiUrl", () => {
   it("should take VERCEL_URL if present and url is relative", () => {
     process.env.VERCEL_URL = "example.com";
 
-    expect(resolveMaybeUrlArg("/foo/bar").href).toBe(
+    expect(Effect.runSync(resolveMaybeUrlArg("/foo/bar")).href).toBe(
       "https://example.com/foo/bar",
     );
 
@@ -38,19 +39,19 @@ describe("getFullApiUrl", () => {
   });
 
   it("assumes localhost if no VERCEL_URL and url is relative", () => {
-    expect(resolveMaybeUrlArg("/foo/bar").href).toBe(
+    expect(Effect.runSync(resolveMaybeUrlArg("/foo/bar")).href).toBe(
       "http://localhost:3000/foo/bar",
     );
   });
 
   it("should use `/api/uploadthing` pathname if no url is provided", () => {
-    expect(resolveMaybeUrlArg("").href).toBe(
+    expect(Effect.runSync(resolveMaybeUrlArg("")).href).toBe(
       "http://localhost:3000/api/uploadthing",
     );
   });
 
   it("should add `/api/uploadthing` if url only has a origin", () => {
-    expect(resolveMaybeUrlArg("http://example.com").href).toBe(
+    expect(Effect.runSync(resolveMaybeUrlArg("http://example.com")).href).toBe(
       "http://example.com/api/uploadthing",
     );
   });
