4e1cb0d2ccd9ac0ef4917e318d5c29c35d42cfda
Julius Marminge <julius0216@outlook.com>
2024-02-28 20:28:00 +0100

    feat(client): add `skipPolling` option (#642)
    Co-authored-by: Mark R. Florkowski <mark.florkowski@gmail.com>

--- packages/react/src/useUploadThing.ts
+++ packages/react/src/useUploadThing.ts
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 
 import type { EndpointMetadata } from "@uploadthing/shared";
 import { UploadThingError } from "@uploadthing/shared";
+import type { UploadFilesOptions } from "uploadthing/client";
 import {
   DANGEROUS__uploadFiles,
   INTERNAL_DO_NOT_USE__fatalClientError,
@@ -41,9 +42,12 @@ export const INTERNAL_uploadthingHookGen = <
    */
   url: URL;
 }) => {
-  const useUploadThing = <TEndpoint extends keyof TRouter>(
+  const useUploadThing = <
+    TEndpoint extends keyof TRouter,
+    TSkipPolling extends boolean = false,
+  >(
     endpoint: TEndpoint,
-    opts?: UseUploadthingProps<TRouter, TEndpoint>,
+    opts?: UseUploadthingProps<TRouter, TEndpoint, TSkipPolling>,
   ) => {
     const [isUploading, setUploading] = useState(false);
     const uploadProgress = useRef(0);
@@ -66,9 +70,14 @@ export const INTERNAL_uploadthingHookGen = <
       setUploading(true);
       opts?.onUploadProgress?.(0);
       try {
-        const res = await DANGEROUS__uploadFiles<TRouter, TEndpoint>(endpoint, {
+        const res = await DANGEROUS__uploadFiles<
+          TRouter,
+          TEndpoint,
+          TSkipPolling
+        >(endpoint, {
           files,
           input,
+          skipPolling: opts?.skipPolling,
           onUploadProgress: (progress) => {
             if (!opts?.onUploadProgress) return;
             fileProgress.current.set(progress.file, progress.progress);
@@ -130,10 +139,13 @@ export const generateReactHelpers = <TRouter extends FileRouter>(
 
   return {
     useUploadThing: INTERNAL_uploadthingHookGen<TRouter>({ url }),
-    uploadFiles: <TEndpoint extends keyof TRouter>(
+    uploadFiles: <
+      TEndpoint extends keyof TRouter,
+      TSkipPolling extends boolean = false,
+    >(
       endpoint: TEndpoint,
       opts: DistributiveOmit<
-        Parameters<typeof DANGEROUS__uploadFiles<TRouter, TEndpoint>>[1],
+        UploadFilesOptions<TRouter, TEndpoint, TSkipPolling>,
         "url" | "package"
       >,
     ) =>
