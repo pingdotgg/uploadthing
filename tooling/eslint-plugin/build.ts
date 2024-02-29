0feda70103397e5ef4d72232326176106a77e3ca
juliusmarminge <julius0216@outlook.com>
2024-02-29 13:25:39 +0100

    fmt

--- tooling/eslint-plugin/build.ts
+++ tooling/eslint-plugin/build.ts
@@ -1,10 +1,10 @@
-import { readdir } from "fs/promises"
+import { readdir } from "fs/promises";
 
-const files = await readdir("src", { recursive: true })
+const files = await readdir("src", { recursive: true });
 
 await Bun.build({
-    entrypoints: files.filter(file => file.endsWith(".ts")),
-    target: "node",
-    external: ["*"],
-    outdir: "dist",
-})
+  entrypoints: files.filter((file) => file.endsWith(".ts")),
+  target: "node",
+  external: ["*"],
+  outdir: "dist",
+});
