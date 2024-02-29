032027e07ad8a387ce6e59db1341b11658ca14cf
juliusmarminge <julius0216@outlook.com>
2024-02-29 13:22:34 +0100

    some fixes

---
+++ tooling/eslint-plugin/build.ts
@@ -0,0 +1,10 @@
+import { readdir } from "fs/promises"
+
+const files = await readdir("src", { recursive: true })
+
+await Bun.build({
+    entrypoints: files.filter(file => file.endsWith(".ts")),
+    target: "node",
+    external: ["*"],
+    outdir: "dist",
+})
