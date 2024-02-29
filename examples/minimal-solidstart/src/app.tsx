415e29d8bc61b6052acd7a3272c1952a931ac3e3
Julius Marminge <julius0216@outlook.com>
2024-02-29 11:35:51 +0100

    chore: update solid to use new start (#651)

---
+++ examples/minimal-solidstart/src/app.tsx
@@ -0,0 +1,24 @@
+// @refresh reload
+import "./global.css";
+
+import { MetaProvider, Title } from "@solidjs/meta";
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start";
+import { Suspense } from "solid-js";
+
+import { generateUploadButton } from "@uploadthing/solid";
+
+export default function App() {
+  return (
+    <Router
+      root={(props) => (
+        <MetaProvider>
+          <Title>SolidStart - Basic</Title>
+          <Suspense>{props.children}</Suspense>
+        </MetaProvider>
+      )}
+    >
+      <FileRoutes />
+    </Router>
+  );
+}
