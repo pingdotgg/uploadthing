3d34c167f8399c36d254c01d60a3222389b6ec62
Julius Marminge <julius0216@outlook.com>
2024-03-05 08:42:03 +0100

    chore: setup playwright e2e (#667)

---
+++ examples/backend-adapters/playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./test",
+  use: {
+    ...devices["Desktop Chrome"],
+    baseURL: "http://localhost:5173",
+    headless: !!process.env.CI,
+  },
+  webServer: [
+    {
+      command: "bun dev:hono",
+      url: "http://localhost:3000/api",
+      reuseExistingServer: !process.env.CI,
+      stdout: "pipe",
+      stderr: "pipe",
+    },
+  ],
+});
