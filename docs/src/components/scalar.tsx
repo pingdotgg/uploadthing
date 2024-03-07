bf0e0c88889dfc2f2e2b00eef3085170665b1620
benox <benbudnevich@gmail.com>
2024-03-07 10:34:11 -0500

    refactor(docs): remove unnecessary useEffect for openapi spec

--- docs/src/components/scalar.tsx
+++ docs/src/components/scalar.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { ApiReference as VueComponent } from "@scalar/api-reference";
 import { useTheme } from "nextra-theme-docs";
 import { applyVueInReact } from "veaury";
@@ -10,14 +9,6 @@ const specUrl = "https://uploadthing.com/api/openapi-spec.json";
 export function ScalarApiRef() {
   const theme = useTheme();
   const isDark = theme.resolvedTheme === "dark";
-
-  useEffect(() => {
-    const toc = document.querySelector('nav[aria-label="table of contents"]');
-    toc?.classList.add("hidden");
-    return () => {
-      toc?.classList.remove("hidden");
-    };
-  }, []);
 
   return (
     // @ts-ignore
