838c242806824f87f1a6f5788f34b1c470cb6bfe
Alan Acuña <alantheace@outlook.com>
2024-04-09 14:30:42 -0600

    feat: Sveltekit support (#225)
    Co-authored-by: juliusmarminge <julius0216@outlook.com>
    Co-authored-by: Mark R. Florkowski <mark.florkowski@gmail.com>

---
+++ packages/svelte/src/lib/utils/createFetch.ts
@@ -0,0 +1,45 @@
+import { onMount } from "svelte";
+import { readonly, writable } from "svelte/store";
+
+interface State<T> {
+  data?: T;
+  error?: Error;
+  type: "fetched" | "error" | "loading";
+}
+
+type Cache<T> = Record<string, T>;
+
+export function createFetch<T = unknown>(url?: string, options?: RequestInit) {
+  const cache: Cache<T> = {};
+  const initialState: State<T> = {
+    type: "loading",
+    error: undefined,
+    data: undefined,
+  };
+  const store = writable(initialState);
+  const fetchData = async () => {
+    if (!url)
+      return store.set({
+        type: "error",
+        error: new Error("No URL provided"),
+      });
+    if (cache[url]) {
+      return store.set({ type: "fetched", data: cache[url] });
+    }
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const data = (await response.json()) as T;
+      cache[url] = data;
+      return store.set({ data, type: "fetched" });
+    } catch (error) {
+      return store.set({ error: error as Error, type: "error" });
+    }
+  };
+  onMount(() => {
+    void fetchData();
+  });
+  return readonly(store);
+}
