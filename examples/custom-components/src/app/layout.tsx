3df628c916c9d77371d8477edcb71db4225dd8ce
juliusmarminge <julius0216@outlook.com>
2024-04-15 23:20:32 +0200

    simple nav

--- examples/custom-components/src/app/layout.tsx
+++ examples/custom-components/src/app/layout.tsx
@@ -2,12 +2,14 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 
+import Link from "next/link";
 import { Toaster } from "sonner";
 import { twMerge } from "tailwind-merge";
 
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 
+import { buttonVariants } from "~/components/ui/button";
 import { uploadRouter } from "~/uploadthing/server";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
@@ -21,6 +23,23 @@ export default function RootLayout({
     <html lang="en">
       <body className={twMerge("font-sans", inter.variable)}>
         <NextSSRPlugin routerConfig={extractRouterConfig(uploadRouter)} />
+        <nav className="w-full border-b-2 border-black p-4 text-center">
+          <Link href="/" className={buttonVariants({ variant: "link" })}>
+            Kitchen-sinky
+          </Link>
+          <Link
+            href="/rhf-builtin"
+            className={buttonVariants({ variant: "link" })}
+          >
+            RHF Using Built-in components
+          </Link>
+          <Link
+            href="/rhf-custom"
+            className={buttonVariants({ variant: "link" })}
+          >
+            RHF Using a custom dropzone component
+          </Link>
+        </nav>
         {children}
         <Toaster />
       </body>
