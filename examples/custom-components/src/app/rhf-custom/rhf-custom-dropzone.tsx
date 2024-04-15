fa3d69bc8b6395d04f3921e627a0ab067bf94459
juliusmarminge <julius0216@outlook.com>
2024-04-15 23:11:35 +0200

    break out

--- examples/custom-components/src/app/rhf-custom/rhf-custom-dropzone.tsx
+++ examples/custom-components/src/app/rhf-custom/rhf-custom-dropzone.tsx
@@ -6,35 +6,79 @@ import { twMerge } from "tailwind-merge";
 import { z } from "zod";
 
 import { useDropzone } from "@uploadthing/react";
+import { FileWithState } from "@uploadthing/shared";
 
 import { Button } from "~/components/ui/button";
-import { Form, FormField, FormItem, useForm } from "~/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  useForm,
+} from "~/components/ui/form";
 import { useUploadThing } from "~/uploadthing/client";
 import { fileWithStateValidator } from "~/utils";
 
+const MyDropzone = (props: {
+  files: FileWithState[];
+  onFilesChange: (files: FileWithState[]) => void;
+}) => {
+  const { routeConfig, startUpload } = useUploadThing("imageUploader", {
+    files: props.files,
+    onFilesChange: props.onFilesChange,
+  });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    routeConfig,
+    onDrop: (files) => {
+      const updatedFiles = [...props.files, ...files];
+      props.onFilesChange(updatedFiles);
+      startUpload(updatedFiles);
+    },
+  });
+
+  return (
+    <div
+      {...getRootProps()}
+      className={twMerge(
+        "border-muted-foreground/25 hover:bg-muted/25 group relative grid h-52 w-full cursor-pointer place-items-center rounded-lg border-2 border-dashed px-5 py-2.5 text-center transition",
+        "ring-offset-background focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
+        isDragActive && "border-muted-foreground/50",
+      )}
+    >
+      <input {...getInputProps()} />
+      <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
+        <div className="rounded-full border border-dashed p-3">
+          <UploadIcon
+            className="text-muted-foreground size-7"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="space-y-px">
+          <p className="font-medium">
+            Drag {`'n'`} drop files here, or click to select files
+          </p>
+          <p className="text-muted-foreground/70 text-sm">
+            You can upload: {Object.keys(routeConfig ?? {}).join(", ")}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 /**
- * A demo using RHF with built-in components from UT
+ * A demo using RHF with a custom dropzone components
  */
 export const SimpleRHFDemo = () => {
   const form = useForm({
     schema: z.object({
       images: fileWithStateValidator.array(),
     }),
-  });
-
-  const { routeConfig, startUpload } = useUploadThing("imageUploader", {
-    files: form.getValues("images"),
-    onFilesChange: (files) => {
-      form.setValue("images", files, { shouldValidate: true });
+    defaultValues: {
+      images: [],
     },
-  });
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    routeConfig,
-    onDrop: (files) => {
-      const updatedFiles = [...form.getValues("images"), ...files];
-      form.setValue("images", updatedFiles);
-      startUpload(updatedFiles);
-    },
+    mode: "onChange",
   });
 
   const onSubmit = form.handleSubmit((data) => {
@@ -58,33 +102,13 @@ export const SimpleRHFDemo = () => {
             name="images"
             render={({ field }) => (
               <FormItem>
-                <div
-                  {...getRootProps()}
-                  className={twMerge(
-                    "border-muted-foreground/25 hover:bg-muted/25 group relative grid h-52 w-full cursor-pointer place-items-center rounded-lg border-2 border-dashed px-5 py-2.5 text-center transition",
-                    "ring-offset-background focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
-                    isDragActive && "border-muted-foreground/50",
-                  )}
-                >
-                  <input {...getInputProps()} />
-                  <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
-                    <div className="rounded-full border border-dashed p-3">
-                      <UploadIcon
-                        className="text-muted-foreground size-7"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <div className="space-y-px">
-                      <p className="font-medium">
-                        Drag {`'n'`} drop files here, or click to select files
-                      </p>
-                      <p className="text-muted-foreground/70 text-sm">
-                        You can upload:{" "}
-                        {Object.keys(routeConfig ?? {}).join(", ")}
-                      </p>
-                    </div>
-                  </div>
-                </div>
+                <FormLabel>Images</FormLabel>
+                <FormControl>
+                  <MyDropzone
+                    files={field.value}
+                    onFilesChange={field.onChange}
+                  />
+                </FormControl>
               </FormItem>
             )}
           />
