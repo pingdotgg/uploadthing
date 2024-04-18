3e0b005c03fa4de55fd0f622212d4532864acde2
juliusmarminge <julius0216@outlook.com>
2024-04-18 13:00:13 +0200

    refactor a bit

--- examples/minimal-expo/app/api/trpc/[trpc]+api.ts
+++ examples/minimal-expo/app/api/trpc/[trpc]+api.ts
@@ -1,17 +1,21 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { z } from "zod";
 
 import { UTApi } from "uploadthing/server";
 
-const createContext = () => ({ utapi: new UTApi() });
+const createContext = () => ({ utapi: new UTApi(), userId: "abc123" });
 const t = initTRPC.context<typeof createContext>().create();
 
 const protectedProcedure = t.procedure.use(({ ctx, next }) => {
   /**
-   * Note to readers: You should do auth here to prevent unauthorized access
-   * to the UTApi
+   * Note to readers: You should do actual auth here to
+   * prevent unauthorized access to the UTApi
    */
+  if (!ctx.userId) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
   return next();
 });
 
