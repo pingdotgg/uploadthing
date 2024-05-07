d62ec24b2358134c393462a46385575128f09a64
juliusmarminge <julius0216@outlook.com>
2024-05-07 22:59:10 +0200

    update some stuff, use libsql over better-sqlite

--- examples/with-drizzle-pagesdir/src/server/db/index.ts
+++ examples/with-drizzle-pagesdir/src/server/db/index.ts
@@ -1,7 +1,8 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
 
+import { env } from "~/env.mjs";
 import * as schema from "./schema";
 
-const sqlite = new Database(".data/sqlite.db");
+const sqlite = createClient({ url: env.DATABASE_URL });
 export const db = drizzle(sqlite, { schema });
