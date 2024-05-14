37de2b41f9fcd1f53ffd9b4dc97aa2ced1db0fd1
juliusmarminge <julius0216@outlook.com>
2024-05-14 12:36:12 +0200

    fmt

--- packages/shared/src/effect.ts
+++ packages/shared/src/effect.ts
@@ -85,9 +85,10 @@ export const parseRequestJson = (req: Request) =>
  * Schedule that retries with exponential backoff, up to 1 minute.
  * 10ms * 4^n, where n is the number of retries.
  */
-export const exponentialBackoff = () => pipe(
-  Schedule.exponential(Duration.millis(10), 4), // 10ms, 40ms, 160ms, 640ms...
-  Schedule.andThenEither(Schedule.spaced(Duration.seconds(1))),
-  Schedule.compose(Schedule.elapsed),
-  Schedule.whileOutput(Duration.lessThanOrEqualTo(Duration.minutes(1))),
-);
+export const exponentialBackoff = () =>
+  pipe(
+    Schedule.exponential(Duration.millis(10), 4), // 10ms, 40ms, 160ms, 640ms...
+    Schedule.andThenEither(Schedule.spaced(Duration.seconds(1))),
+    Schedule.compose(Schedule.elapsed),
+    Schedule.whileOutput(Duration.lessThanOrEqualTo(Duration.minutes(1))),
+  );
