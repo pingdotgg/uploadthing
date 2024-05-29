acc5d647344a63091f55f25c0b562e6b359e0b4c
Mark R. Florkowski <mark.florkowski@gmail.com>
2024-05-29 14:03:30 -0700

    typecheck

--- packages/react/src/useUploadThing.ts
+++ packages/react/src/useUploadThing.ts
@@ -82,7 +82,7 @@ export const INTERNAL_uploadthingHookGen = <
       const input = args[1];
 
       setUploading(true);
-      opts?.onUploadProgress?.(0);
+      opts?.onUploadProgress?.(0, undefined);
       try {
         const res = await uploadFiles<TEndpoint, TSkipPolling>(endpoint, {
           headers: opts?.headers,
@@ -98,7 +98,7 @@ export const INTERNAL_uploadthingHookGen = <
             const averageProgress =
               Math.floor(sum / fileProgress.current.size / 10) * 10;
             if (averageProgress !== uploadProgress.current) {
-              opts?.onUploadProgress?.(averageProgress);
+              opts?.onUploadProgress?.(averageProgress, progress);
               uploadProgress.current = averageProgress;
             }
           },
