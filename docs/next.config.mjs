bf278b1a39db95e1bd8987e35c40e35065d91a3c
Mark R. Florkowski <mark.florkowski@gmail.com>
2024-05-31 13:42:24 -0700

    chore: Add redirects to docs nextconfig (#843)

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -8,6 +8,35 @@ const withNextra = nextra({
   flexsearch: {
     codeblock: false,
   },
+  async redirects() {
+    return [
+      {
+        source: "/getting-started",
+        destination: "/getting-started/appdir",
+        permanent: true,
+      },
+      {
+        source: "/nextjs/appdir",
+        destination: "/getting-started/appdir",
+        permanent: true,
+      },
+      {
+        source: "/nextjs/pagedir",
+        destination: "/getting-started/pagedir",
+        permanent: true,
+      },
+      {
+        source: "/solid",
+        destination: "/getting-started/solid",
+        permanent: true,
+      },
+      {
+        source: "/solidstart/server",
+        destination: "/getting-started/solid",
+        permanent: true,
+      },
+    ];
+  },
 });
 
 export default withNextra({
