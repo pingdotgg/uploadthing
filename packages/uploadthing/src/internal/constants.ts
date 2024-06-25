58f3ee4c632e73700636f224823a1400ad92cb3c
juliusmarminge <julius0216@outlook.com>
2024-06-25 10:32:55 +0200

    eh?

--- packages/uploadthing/src/internal/constants.ts
+++ packages/uploadthing/src/internal/constants.ts
@@ -1,6 +1,6 @@
 export { version as UPLOADTHING_VERSION } from "../../package.json";
 
-// export const INGEST_URL = "https://fra1-dev.api.uploadthing.com";
-export const INGEST_URL = "http://localhost:3001";
+export const INGEST_URL = "https://fra1-dev.api.uploadthing.com";
+// export const INGEST_URL = "http://localhost:3001";
 
 export const DEFAULT_PRESIGNED_URL_TTL = 60 * 60;
