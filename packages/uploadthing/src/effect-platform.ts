14adb1874bc089b438fa74dd0475826ea62f13ba
juliusmarminge <julius0216@outlook.com>
2024-07-02 10:53:39 +0200

    callout

--- packages/uploadthing/src/effect-platform.ts
+++ packages/uploadthing/src/effect-platform.ts
@@ -4,7 +4,7 @@ import * as Effect from "effect/Effect";
 import type { Json } from "@uploadthing/shared";
 
 import { createRequestHandler, MiddlewareArguments } from "./internal/handler";
-import type { FileRouter, RouteHandlerOptions } from "./internal/types";
+import type { FileRouter, RouteHandlerConfig } from "./internal/types";
 import type { CreateBuilderOptions } from "./internal/upload-builder";
 import { createBuilder } from "./internal/upload-builder";
 
@@ -21,9 +21,29 @@ export const createUploadthing = <TErrorShape extends Json>(
   opts?: CreateBuilderOptions<TErrorShape>,
 ) => createBuilder<MiddlewareArgs, TErrorShape>(opts);
 
-export const createRouteHandler = <TRouter extends FileRouter>(
-  opts: RouteHandlerOptions<TRouter>,
-) => {
+export const createRouteHandler = <TRouter extends FileRouter>(opts: {
+  router: TRouter;
+  /**
+   * @remarks In order to obey by Effect conventions, we have omitted the `config.fetch` and `config.logLevel` options.
+   * You can provide these layers on your own if you need to.
+   *
+   * @example
+   * ```ts
+   * import { Effect, Layer, Logger, LogLevel } from "effect";
+   * import { HttpClient } from "@effect/platform";
+
+   * // Set logLevel
+   * Logger.withMinimumLogLevel(LogLevel.Debug)
+   *   
+   * // Override fetch implementation
+   * Layer.succeed(
+   *   HttpClient.Fetch,
+   *   myFetchImplementation,
+   * );
+   * ```
+   */
+  config?: Omit<RouteHandlerConfig, "fetch" | "logLevel">;
+}) => {
   // const handler = makeThing<[HttpServerRequest]>(
   //   (_) => Effect.succeed({ req: _, res: undefined, event: undefined }),
   //   (_) => _.source as Request,
