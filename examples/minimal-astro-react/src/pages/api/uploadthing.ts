08f5c56b27d34c8f4fde3b4a67b54edd1b10e8d2
juliusmarminge <julius0216@outlook.com>
2024-07-17 12:24:47 +0200

    fmt

--- examples/minimal-astro-react/src/pages/api/uploadthing.ts
+++ examples/minimal-astro-react/src/pages/api/uploadthing.ts
@@ -5,7 +5,7 @@ import { uploadRouter } from "~/server/uploadthing";
 /**
  * This example uses the hybrid mode so we must opt-in to dynamic rendering
  * https://docs.astro.build/en/guides/endpoints/#server-endpoints-api-routes
- **/ 
+ **/
 export const prerender = false;
 
 export const ALL = createRouteHandler({
