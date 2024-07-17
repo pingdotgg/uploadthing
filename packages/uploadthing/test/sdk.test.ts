28ddea06ee38edbb0925ca9518df149caad6c408
juliusmarminge <julius0216@outlook.com>
2024-07-17 14:35:46 +0200

    no debuggy

--- packages/uploadthing/test/sdk.test.ts
+++ packages/uploadthing/test/sdk.test.ts
@@ -414,7 +414,6 @@ describe.runIf(shouldRun)(
       token: shouldRun
         ? process.env.UPLOADTHING_TEST_TOKEN!
         : testToken.encoded,
-      logLevel: "Debug",
     });
 
     const localInfo = { totalBytes: 0, filesUploaded: 0 };
