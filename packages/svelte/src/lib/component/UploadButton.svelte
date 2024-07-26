aeeb44f462c12a551c670bc85ab2abbba523090c
Mark R. Florkowski <mark.florkowski@gmail.com>
2024-07-26 15:18:31 -0700

    fix logic

--- packages/svelte/src/lib/component/UploadButton.svelte
+++ packages/svelte/src/lib/component/UploadButton.svelte
@@ -228,13 +228,15 @@ Example:
     />
     <slot name="button-content" state={styleFieldArg}>
       {#if state !== "uploading"}
-        <span>`Choose File${multiple ? `(s)` : ``}`</span>
-      {:else if state !== "uploading" && mode === "manual" && files.length > 0}
-        <span>
-          `Upload ${files.length} file${files.length === 1 ? "" : "s"}`
-        </span>
-      {:else if state !== "uploading" && fileTypes.length === 0}
-        <span>Loading...</span>
+        {#if fileTypes.length === 0}
+          <span>Loading...</span>
+        {:else if mode === "manual" && files.length > 0}
+          <span>
+            `Upload ${files.length} file${files.length === 1 ? "" : "s"}`
+          </span>
+        {:else}
+          <span>`Choose File${multiple ? `(s)` : ``}`</span>
+        {/if}
       {:else if uploadProgress === 100}
         <Spinner />
       {:else}
