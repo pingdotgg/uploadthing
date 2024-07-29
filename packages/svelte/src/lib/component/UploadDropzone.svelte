d09c2e2423a988d7a4327a47f3a7882b4df0f756
Mark R. Florkowski <mark.florkowski@gmail.com>
2024-07-29 14:55:59 -0700

    no cursor-pointer on label

--- packages/svelte/src/lib/component/UploadDropzone.svelte
+++ packages/svelte/src/lib/component/UploadDropzone.svelte
@@ -235,7 +235,7 @@
   </slot>
   <label
     class={twMerge(
-      "relative mt-4 flex w-64 cursor-pointer items-center justify-center text-sm font-semibold leading-6 text-gray-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2 hover:text-blue-500",
+      "relative mt-4 flex w-64 items-center justify-center text-sm font-semibold leading-6 text-gray-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2 hover:text-blue-500",
       ready && state !== "disabled"
         ? "text-blue-600"
         : "text-gray-500 hover:text-gray-500",
