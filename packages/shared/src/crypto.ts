62948d26b4309ff70a3bc2fb6f955e01984a9d45
juliusmarminge <julius0216@outlook.com>
2024-08-25 23:54:22 +0200

    rm Buffer refernence

--- packages/shared/src/crypto.ts
+++ packages/shared/src/crypto.ts
@@ -1,3 +1,5 @@
+import * as Encoding from "effect/Encoding";
+
 const signaturePrefix = "hmac-sha256=";
 const algorithm = { name: "HMAC", hash: "SHA-256" };
 
@@ -13,7 +15,7 @@ export const signPayload = async (payload: string, secret: string) => {
 
   const signature = await crypto.subtle
     .sign(algorithm, signingKey, encoder.encode(payload))
-    .then((sig) => Buffer.from(sig).toString("hex"));
+    .then((arrayBuffer) => Encoding.encodeHex(new Uint8Array(arrayBuffer)));
 
   return `${signaturePrefix}${signature}`;
 };
