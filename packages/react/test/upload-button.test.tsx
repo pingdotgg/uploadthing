ebf58a27e1f35f4207798452cc2818c9a7153cd5
Mark R. Florkowski <mark.florkowski@gmail.com>
2024-09-06 14:48:57 -0700

    types still broken, but some fixes for test

--- packages/react/test/upload-button.test.tsx
+++ packages/react/test/upload-button.test.tsx
@@ -34,12 +34,14 @@ const testRouter = {
 const routeHandler = createRouteHandler({ router: testRouter });
 const UploadButton = generateUploadButton<typeof testRouter>();
 
-const utGet = vi.fn<[Request]>();
-const utPost = vi.fn<[{ request: Request; body: any }]>();
+const utGet = vi.fn<(req: Request) => void>();
+const utPost =
+  vi.fn<({ request, body }: { request: Request; body: any }) => void>();
+
 const server = setupServer(
   http.get("/api/uploadthing", ({ request }) => {
     utGet(request);
-    return routeHandler.GET(request);
+    return routeHandler(request);
   }),
   http.post("/api/uploadthing", async ({ request }) => {
     const body = await request.json();
@@ -91,6 +93,8 @@ describe("UploadButton - basic", () => {
   it("fetches and displays route config", async () => {
     const utils = render(<UploadButton endpoint="image" />);
     const label = utils.container.querySelector("label");
+
+    if (!label) throw new Error("No label found");
 
     // Previously, when component was disabled, it would show "Loading..."
     // expect(label).toHaveTextContent("Loading...");
