9e5d41607a37dce715b79ae39997cb22420a8030
juliusmarminge <julius0216@outlook.com>
2024-09-25 09:57:54 +0200

    rm overrides

--- examples/minimal-solidstart/tailwind.config.js
+++ examples/minimal-solidstart/tailwind.config.js
@@ -2,10 +2,7 @@ import { withUt } from "uploadthing/tw";
 
 /** @type {import('tailwindcss').Config} */
 export default withUt({
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@uploadthing/solid/dist/**/*.js", // Remove this once #975 is resolved
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
