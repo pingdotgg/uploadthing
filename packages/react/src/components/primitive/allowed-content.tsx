1eb1b3130893bba4653e106dcbf0a7c1764dd02d
veloii <85405932+veloii@users.noreply.github.com>
2024-09-26 19:51:50 +0100

    feat: wrap primitive allowed content in a div

--- packages/react/src/components/primitive/allowed-content.tsx
+++ packages/react/src/components/primitive/allowed-content.tsx
@@ -1,18 +1,23 @@
 import { allowedContentTextLabelGenerator } from "@uploadthing/shared";
 
 import {
-  PrimitiveComponentChildrenProp,
+  PrimitiveComponentProps,
   PrimitiveSlot,
   usePrimitiveValues,
 } from "./root";
 
-export function AllowedContent(props: PrimitiveComponentChildrenProp) {
-  const { routeConfig } = usePrimitiveValues("AllowedContent");
+export function AllowedContent({
+  children,
+  ...props
+}: PrimitiveComponentProps<"div">) {
+  const { routeConfig, state } = usePrimitiveValues("AllowedContent");
 
   return (
-    <PrimitiveSlot
-      default={allowedContentTextLabelGenerator(routeConfig)}
-      children={props.children}
-    />
+    <div data-state={state} {...props}>
+      <PrimitiveSlot
+        default={allowedContentTextLabelGenerator(routeConfig)}
+        children={children}
+      />
+    </div>
   );
 }
