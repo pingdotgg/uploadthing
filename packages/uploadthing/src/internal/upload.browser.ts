56aaa52c9214fcdb116e7a791a1be0823acddec0
juliusmarminge <julius0216@outlook.com>
2024-10-14 22:45:36 +0200

    make sure slicing retains name and type

--- packages/uploadthing/src/internal/upload.browser.ts
+++ packages/uploadthing/src/internal/upload.browser.ts
@@ -57,8 +57,13 @@ const uploadWithProgress = (
     });
 
     const formData = new FormData();
-    console.log("appending file to FD", file, rangeStart)
-    formData.append("file", file.slice(rangeStart));
+    formData.append(
+      "file",
+      new File([file.slice(rangeStart)], file.name, {
+        lastModified: file.lastModified,
+        type: file.type,
+      }),
+    );
     xhr.send(formData);
 
     return Micro.sync(() => xhr.abort());
