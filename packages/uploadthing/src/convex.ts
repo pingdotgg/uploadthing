34749249e48a603de4cf823c4281ff09d5e3901d
juliusmarminge <julius0216@outlook.com>
2024-10-17 10:19:19 +0200

    v7

--- packages/uploadthing/src/convex.ts
+++ packages/uploadthing/src/convex.ts
@@ -1,126 +1,113 @@
-import { createBuilder, type CreateBuilderOptions } from "./internal/upload-builder";
-import { FileRouterInputConfig, getStatusCodeFromError, type Json } from "@uploadthing/shared";
-import { type FileRouter, RouteHandlerOptions, UnsetMarker, UploadBuilder, Uploader } from "./internal/types";
-import { buildPermissionsInfoHandler, buildRequestHandler, runRequestHandlerAsync } from "./internal/handler";
-import { DataModelFromSchemaDefinition, GenericActionCtx, GenericDataModel, httpActionGeneric, HttpRouter, SchemaDefinition } from "convex/server"
-import { UPLOADTHING_VERSION } from "./internal/constants";
-import { formatError } from "./internal/error-formatter";
+import type {
+  DataModelFromSchemaDefinition,
+  GenericActionCtx,
+  GenericDataModel,
+  HttpRouter,
+  SchemaDefinition,
+} from "convex/server";
+import { httpActionGeneric, httpRouter } from "convex/server";
+import * as Effect from "effect/Effect";
+
+import type { Json } from "@uploadthing/shared";
+
+import { makeAdapterHandler } from "./internal/handler";
+import type { FileRouter, RouteHandlerOptions } from "./internal/types";
+import { createBuilder } from "./internal/upload-builder";
+import type { CreateBuilderOptions } from "./internal/upload-builder";
 
 export type { FileRouter };
 
-type MiddlewareArgs<DataModel extends GenericDataModel> = { req: Request, res: undefined; event: undefined };
+type MiddlewareArgs<DataModel extends GenericDataModel> = {
+  req: Request;
+  res: undefined;
+  event: GenericActionCtx<DataModel>;
+};
 
-type ConvexBuilderOptions<TErrorShape extends Json, SchemaDef extends SchemaDefinition<any, boolean>> = CreateBuilderOptions<TErrorShape> & {
-  schema?: SchemaDef
-}
+type ConvexBuilderOptions<
+  TErrorShape extends Json,
+  SchemaDef extends SchemaDefinition<any, boolean>,
+> = CreateBuilderOptions<TErrorShape> & {
+  schema?: SchemaDef;
+};
 
-export const createUploadthing = <TErrorShape extends Json, SchemaDef extends SchemaDefinition<any, boolean>>(
+export const createUploadthing = <
+  TErrorShape extends Json,
+  SchemaDef extends SchemaDefinition<any, boolean>,
+>(
   opts?: ConvexBuilderOptions<TErrorShape, SchemaDef>,
-) => createBuilder<MiddlewareArgs<DataModelFromSchemaDefinition<SchemaDef>>, TErrorShape>(opts);
-
-export function convexCtx<DataModel extends GenericDataModel>(
-  f: (input: FileRouterInputConfig) => UploadBuilder<{
-    _input: any,
-    _metadata: any,
-    _middlewareArgs: MiddlewareArgs<DataModel>,
-    _errorShape: any,
-    _errorFn: any,
-    _output: any,
-  }>
-): GenericActionCtx<DataModel> {
-  const fAny = f as any;
-  if (!fAny.ctx) {
-    throw new Error("convexCtx() called outside of an Uploadthing route");
-  }
-  return fAny.ctx;
-}
+) =>
+  createBuilder<
+    MiddlewareArgs<DataModelFromSchemaDefinition<SchemaDef>>,
+    TErrorShape
+  >(opts);
 
 export const addUploadthingRoutes = <TRouter extends FileRouter>(
   router: HttpRouter,
-  f: (input: FileRouterInputConfig) => UploadBuilder<any>,
   opts: RouteHandlerOptions<TRouter>,
 ) => {
-  const requestHandler = buildRequestHandler<TRouter, MiddlewareArgs<GenericDataModel>>(
+  const handler = makeAdapterHandler<
+    [GenericActionCtx<GenericDataModel>, Request]
+  >(
+    (ctx, req) => Effect.succeed({ req, res: undefined, event: ctx }),
+    (_, req) => Effect.succeed(req),
     opts,
     "convex",
   );
-  const getBuildPerms = buildPermissionsInfoHandler<TRouter>(opts);
-  const fAny = f as any;
+
+  router.route({
+    method: "GET",
+    path: "/api/uploadthing",
+    handler: httpActionGeneric(handler),
+  });
+
+  router.route({
+    method: "POST",
+    path: "/api/uploadthing",
+    handler: httpActionGeneric(handler),
+  });
 
   router.route({
     method: "OPTIONS",
     path: "/api/uploadthing",
-    handler: httpActionGeneric(async (_ctx, req) => {
-      const { headers } = req;
-      const isCorsRequest = headers.get("Origin") != null
-        && headers.get("Access-Control-Request-Method") != null
-        && headers.get("Access-Control-Request-Headers") != null;
+    handler: httpActionGeneric((_ctx, { headers }) => {
+      const isCorsRequest =
+        headers.get("Origin") != null &&
+        headers.get("Access-Control-Request-Method") != null &&
+        headers.get("Access-Control-Request-Headers") != null;
+
       if (!isCorsRequest) {
-        return new Response();
+        return Promise.resolve(new Response());
       }
-      return new Response(null, {
-        status: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-          "Access-Control-Allow-Headers": "*",
-          "Access-Control-Max-Age": "86400",
-        }
-      })
-    })
-  })
-  router.route({
-    method: "GET",
-    path: "/api/uploadthing",
-    handler: httpActionGeneric(async () => {
-      const permissions = getBuildPerms();
-      return new Response(JSON.stringify(permissions), {
-        headers: {
-          "X-Uploadthing-Version": UPLOADTHING_VERSION,
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Vary": "Origin",
-        }
-      });
-    })
-  })
-  router.route({
-    method: "POST",
-    path: "/api/uploadthing",
-    handler: httpActionGeneric(async (ctx, req) => {
-      const headers = {
-        "X-Uploadthing-Version": UPLOADTHING_VERSION,
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Vary": "Origin",
-      }
-      try {
-        fAny.ctx = ctx;
-        const response = await runRequestHandlerAsync(
-          requestHandler,
-          {
-            req,
-            middlewareArgs: { req, res: undefined, event: undefined },
+      return Promise.resolve(
+        new Response(null, {
+          status: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Max-Age": "86400",
           },
-          opts.config,
-        )
-        if (response.success === false) {
-          return new Response(JSON.stringify(formatError(response.error, opts.router)), {
-            status: getStatusCodeFromError(response.error),
-            headers,
-          })
-        }
-        if (!response.body) {
-          return new Response(JSON.stringify({ ok: true }), {
-            headers,
-          })
-        }
-        return new Response(JSON.stringify(response.body), {
-          headers,
-        })
-      } finally {
-        delete fAny.ctx;
-      }
+        }),
+      );
+    }),
+  });
+};
+
+//
+// TEST
+//
+
+const f = createUploadthing();
+const router = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async ({ event }) => {
+      const identity = await event.auth.getUserIdentity();
+      return { userId: identity?.subject ?? "nothing" };
     })
-  })
-}
+    .onUploadComplete(async (args) => {
+      return { uploadedBy: args.metadata.userId };
+    }),
+} satisfies FileRouter;
+
+const http = httpRouter();
+addUploadthingRoutes(http, { router });
