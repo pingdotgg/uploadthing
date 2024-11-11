ba738baae20a15bb88e39d3ee2acdb771d0a0264
juliusmarminge <julius0216@outlook.com>
2024-11-11 23:56:36 +0100

    export the generic FileRoute type

--- packages/uploadthing/src/types.ts
+++ packages/uploadthing/src/types.ts
@@ -8,7 +8,7 @@ import type {
 } from "@uploadthing/shared";
 
 import type { LogFormat } from "./internal/logger";
-import type { AnyFileRoute } from "./internal/types";
+import type { AnyFileRoute, FileRoute } from "./internal/types";
 
 export * from "./sdk/types";
 
@@ -25,7 +25,7 @@ export type {
   NewPresignedUrl,
 } from "./internal/shared-schemas";
 
-export type { AnyFileRoute };
+export type { FileRoute, AnyFileRoute };
 export type FileRouter = Record<string, AnyFileRoute>;
 
 export type inferEndpointInput<TFileRoute extends AnyFileRoute> =
