98582429d8fb664c18ff07b7eab0829ba7c132f3
juliusmarminge <julius0216@outlook.com>
2024-11-18 23:08:49 +0100

    ok

--- packages/uploadthing/src/sdk/index.ts
+++ packages/uploadthing/src/sdk/index.ts
@@ -160,8 +160,7 @@ export class UTApi {
       Effect.withLogSpan("uploadFiles"),
     );
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return this.executeAsync(program, opts?.signal) as any;
+    return this.executeAsync(program, opts?.signal);
   }
 
   /**
