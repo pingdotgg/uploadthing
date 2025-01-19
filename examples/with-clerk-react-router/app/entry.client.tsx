f74d6016e23bb3afaa48d70de9f7ad6d53d17f2f
juliusmarminge <julius0216@outlook.com>
2025-01-19 14:12:18 +0100

    add note about potential need for indirection

--- examples/with-clerk-react-router/app/entry.client.tsx
+++ examples/with-clerk-react-router/app/entry.client.tsx
@@ -5,8 +5,8 @@
  */
 
 import { startTransition, StrictMode } from "react";
-import { HydratedRouter } from "react-router/dom";
 import { hydrateRoot } from "react-dom/client";
+import { HydratedRouter } from "react-router/dom";
 
 startTransition(() => {
   hydrateRoot(
