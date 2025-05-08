dba8678f30a5f4633c795ee70f08ca2c12c7e4f6
juliusmarminge <jmarminge@gmail.com>
2025-05-08 14:09:13 +0200

    fix typeerrros in tests

--- packages/uploadthing/test/node/request-handler.test.ts
+++ packages/uploadthing/test/node/request-handler.test.ts
@@ -410,6 +410,7 @@ describe(".onUploadComplete()", () => {
     const payload = JSON.stringify({
       status: "uploaded",
       metadata: {},
+      origin: "https://example.com",
       file: new UploadedFileData({
         url: `${UTFS_URL}/f/some-random-key.png`,
         appUrl: `${UTFS_URL}/a/${testToken.decoded.appId}/f/some-random-key.png`,
