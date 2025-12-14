fa6a80ca9c5619953599d32ae2e87def92cb62e4
Julius <julius0216@outlook.com>
2025-12-13 21:28:52 -0800

    backend adapters

--- examples/backend-adapters/server/src/custom.ts
+++ examples/backend-adapters/server/src/custom.ts
@@ -6,8 +6,8 @@ import { Effect } from "effect";
 import { createBuilder, makeAdapterHandler } from "uploadthing/server";
 
 type AdapterArgs = {
-  req: BunRequest;
-  server: Server;
+  req: BunRequest<string>;
+  server: Server<unknown>;
 };
 const f = createBuilder<AdapterArgs>();
 
@@ -34,7 +34,10 @@ const router = {
     }),
 };
 
-const requestHandler = makeAdapterHandler<[BunRequest, Server], AdapterArgs>(
+const requestHandler = makeAdapterHandler<
+  [BunRequest<string>, Server<unknown>],
+  AdapterArgs
+>(
   (req, server) => Effect.succeed({ req, server }),
   (req) => Effect.succeed(req),
   {
